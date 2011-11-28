815843abb3bfd5a4548e167520b6e317fac6a17c
Russell Warneboldt <warneboldt@gmail.com>
2011-11-28 15:04:46 -0800

    Adding Languages table migration, with a single entry "English"

---
+++ db/migrate/20111125222926_create_languages.rb
@@ -0,0 +1,12 @@
+class CreateLanguages < ActiveRecord::Migration
+    def change
+        create_table :languages do |t|
+            t.string :name
+            
+            t.timestamps
+        end
+        
+        Language.reset_column_information
+        Language.create :name => "English"
+    end
+end
