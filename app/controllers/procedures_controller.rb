dd4ddaacc4519e3e16d182a812ef85dc2e458275
Russell Warneboldt <warneboldt@gmail.com>
2011-12-04 21:09:07 -0800

    Procedure parenting now correctly only allows parenting to procedures in the same specialization

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,6 +1,6 @@
 class ProceduresController < ApplicationController
   load_and_authorize_resource
-
+  
   def index
     if params[:specialization_id] != nil
       @specialization = Specialization.find params[:specialization_id]
@@ -10,19 +10,19 @@ class ProceduresController < ApplicationController
       redirect_to specialization_path(@specialization) and return
     end
   end
-
+  
   def show
     @procedure = Procedure.find(params[:id])
     if request.headers['X-PJAX']
       render :layout => false
     end
   end
-
+  
   def new
     @specialization = Specialization.find(params[:specialization_id])
     @procedure = Procedure.new :specialization_id => @specialization.id
   end
-
+  
   def create
     @procedure = Procedure.new(params[:procedure])
     if @procedure.save
@@ -31,12 +31,13 @@ class ProceduresController < ApplicationController
       render :action => 'new'
     end
   end
-
+  
   def edit
     @procedure = Procedure.find(params[:id])
-    @procedure_ancestry = ancestry_options(Procedure.scoped.arrange(:order => 'name')) {|i| "#{'-' * i.depth} #{i.name}" }
+    @specialization = Specialization.find(params[:specialization_id])
+    @procedure_ancestry = [["~ No parent ~", nil]] + ancestry_options(@specialization.procedures.arrange(:order => 'name'), @procedure)
   end
-
+  
   def update
     @procedure = Procedure.find(params[:id])
     if @procedure.update_attributes(params[:procedure])
@@ -45,23 +46,24 @@ class ProceduresController < ApplicationController
       render :action => 'edit'
     end
   end
-
+  
   def destroy
     @procedure = Procedure.find(params[:id])
     @procedure.destroy
     # redirect_to '/specializations', :notice => "Successfully destroyed scope of practice."
     redirect_to procedures_url, :notice => "Successfully destroyed area of practice."
   end
+  
+  def ancestry_options(items, skip_item, &block)
+    return ancestry_options(items, skip_item){ |i| "#{'-' * i.depth} #{i.name}" } unless block_given?
     
-    def ancestry_options(items, &block)
-        return ancestry_options(items){ |i| "#{'-' * i.depth} #{i.name}" } unless block_given?
-        
-        result = []
-        items.map do |item, sub_items|
-            result << [yield(item), item.id]
-            #this is a recursive call:
-            result += ancestry_options(sub_items, &block)
-        end
-        result
+    result = []
+    items.map do |item, sub_items|
+      next if item == skip_item
+      result << [yield(item), item.id]
+      #this is a recursive call:
+      result += ancestry_options(sub_items, skip_item, &block)
     end
+    result
+  end
 end
