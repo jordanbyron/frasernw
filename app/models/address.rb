aad349e9d68a755a6e29490f3503341f915be54a
Russell Warneboldt <warneboldt@gmail.com>
2011-12-07 17:12:29 -0800

    Adding address class

---
+++ app/models/address.rb
@@ -0,0 +1,3 @@
+class Address < ActiveRecord::Base
+  has_paper_trail
+end
---
+++ db/migrate/20111207220713_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration
+  def change
+    create_table :addresses do |t|
+      t.string :address1
+      t.string :address2
+      t.string :postalcode
+      t.string :city
+      t.string :province
+      t.string :phone1
+      t.string :fax
+      
+      t.timestamps
+    end
+  end
+end
