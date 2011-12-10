e628285aa45b6c7b574e26e2582e1c36192f0b26
Russell Warneboldt <warneboldt@gmail.com>
2011-12-09 17:27:57 -0800

    adding healthcare _providers and clinic_healthcare_providers tables

---
+++ db/migrate/20111210005346_create_healthcare_providers.rb
@@ -0,0 +1,23 @@
+class CreateHealthcareProviders < ActiveRecord::Migration
+  def change
+    create_table :healthcare_providers do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    
+    HealthcareProvider.reset_column_information
+    HealthcareProvider.create :name => "Nurse"
+    HealthcareProvider.create :name => "Nurse Practitioner"
+    HealthcareProvider.create :name => "Occupational Therapist"
+    HealthcareProvider.create :name => "Physiotherapist"
+    HealthcareProvider.create :name => "Respiratory Therapist"
+    HealthcareProvider.create :name => "Pharmacist"
+    HealthcareProvider.create :name => "Dietician"
+    HealthcareProvider.create :name => "Social Worker"
+    HealthcareProvider.create :name => "Pastoral Care"
+    HealthcareProvider.create :name => "Psychologist"
+    HealthcareProvider.create :name => "Psych nurse"
+    HealthcareProvider.create :name => "Volunteers"
+  end
+end
---
+++ db/migrate/20111210005705_create_clinic_healthcare_provider.rb
@@ -0,0 +1,10 @@
+class CreateClinicHealthcareProvider < ActiveRecord::Migration
+  def change
+    create_table :clinic_healthcare_providers do |t|
+      t.integer :clinic_id
+      t.integer :healthcare_provider_id
+      
+      t.timestamps
+    end
+  end
+end
