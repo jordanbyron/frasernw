6eca88fc7b2dde9696cf1b5fdfb52e93a18b8f08
Russell Warneboldt <warneboldt@gmail.com>
2011-12-10 15:45:06 -0800

    First pass at clinic data entry form

---
+++ app/assets/javascripts/clinic.js
@@ -0,0 +1,14 @@
+$(".is_specialist").live("change", function() {
+  if ( $(this).is(":checked") )
+  {
+    //we have indicated a specialist
+    $(this).siblings(".specialist_list").show();
+    $(this).siblings(".freeform").hide();
+  }
+  else
+  {
+    //we have indicated a freeform answer
+    $(this).siblings(".specialist_list").hide();
+    $(this).siblings(".freeform").show();
+  }
+});
