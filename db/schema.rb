ea56cd76d732440a165dbbc05a5bb4c885d552cf
Russell Warneboldt <warneboldt@gmail.com>
2011-12-10 15:45:18 -0800

    Schema for all the clinic changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,32 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111203201549) do
+ActiveRecord::Schema.define(:version => 20111210034424) do
+
+  create_table "addresses", :force => true do |t|
+    t.string   "address1"
+    t.string   "address2"
+    t.string   "postalcode"
+    t.string   "city"
+    t.string   "province"
+    t.string   "phone1"
+    t.string   "fax"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "hospital_id"
+  end
 
   create_table "attendances", :force => true do |t|
-    t.integer   "specialist_id"
-    t.integer   "clinic_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.integer  "specialist_id"
+    t.integer  "clinic_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "freeform_first_name"
+    t.string   "freeform_last_name"
+    t.boolean  "is_specialist",       :default => true
+    t.string   "freeform_firstname"
+    t.string   "freeform_lastname"
+    t.string   "area_of_focus"
   end
 
   create_table "capacities", :force => true do |t|
@@ -30,24 +49,71 @@ ActiveRecord::Schema.define(:version => 20111203201549) do
 
   add_index "capacities", ["specialist_id", "procedure_id"], :name => "index_capacities_on_specialist_id_and_procedure_id"
 
+  create_table "clinic_addresses", :force => true do |t|
+    t.integer  "clinic_id"
+    t.integer  "address_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "clinic_healthcare_providers", :force => true do |t|
+    t.integer  "clinic_id"
+    t.integer  "healthcare_provider_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "clinic_speaks", :force => true do |t|
+    t.integer  "clinic_id"
+    t.integer  "language_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "clinics", :force => true do |t|
-    t.string    "name"
-    t.string    "address1"
-    t.string    "address2"
-    t.string    "postalcode"
-    t.string    "city"
-    t.string    "province"
-    t.string    "phone1"
-    t.string    "fax"
-    t.text      "status"
-    t.text      "interest"
-    t.integer   "waittime"
-    t.integer   "specialization_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.text      "referral_criteria"
-    t.text      "referral_process"
-    t.string    "responds_via"
+    t.string   "name"
+    t.string   "address1"
+    t.string   "address2"
+    t.string   "postalcode"
+    t.string   "city"
+    t.string   "province"
+    t.string   "phone1"
+    t.string   "fax"
+    t.text     "status"
+    t.text     "interest"
+    t.integer  "waittime"
+    t.integer  "specialization_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "referral_criteria"
+    t.text     "referral_process"
+    t.string   "responds_via"
+    t.string   "contact_name"
+    t.string   "contact_phone"
+    t.string   "contact_email"
+    t.string   "contact_notes"
+    t.integer  "status_mask"
+    t.text     "limitations"
+    t.text     "location_opened"
+    t.text     "required_investigations"
+    t.text     "not_performed"
+    t.boolean  "referral_fax"
+    t.boolean  "referral_phone"
+    t.string   "referral_other_details"
+    t.boolean  "referral_form"
+    t.string   "lagtime"
+    t.string   "lag_uom"
+    t.string   "waitime"
+    t.string   "wait_uom"
+    t.boolean  "respond_by_fax"
+    t.boolean  "respond_by_phone"
+    t.boolean  "respond_by_mail"
+    t.boolean  "respond_to_patient"
+    t.boolean  "patient_can_book"
+    t.text     "red_flags"
+    t.boolean  "urgent_fax"
+    t.boolean  "urgent_phone"
+    t.string   "urgent_other_details"
   end
 
   create_table "contacts", :force => true do |t|
@@ -78,10 +144,19 @@ ActiveRecord::Schema.define(:version => 20111203201549) do
   end
 
   create_table "focuses", :force => true do |t|
-    t.integer   "clinic_id"
-    t.integer   "procedure_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.integer  "clinic_id"
+    t.integer  "procedure_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "investigation"
+  end
+
+  add_index "focuses", ["clinic_id", "procedure_id"], :name => "index_focuses_on_clinic_id_and_procedure_id"
+
+  create_table "healthcare_providers", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "hospitals", :force => true do |t|
@@ -147,6 +222,16 @@ ActiveRecord::Schema.define(:version => 20111203201549) do
 
   add_index "procedures", ["ancestry"], :name => "index_procedures_on_ancestry"
 
+  create_table "review_items", :force => true do |t|
+    t.string   "whodunnit"
+    t.text     "object"
+    t.integer  "specialist_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "review_items", ["specialist_id"], :name => "index_review_items_on_specialist_id"
+
   create_table "reviews", :force => true do |t|
     t.string    "item_type",      :null => false
     t.integer   "item_id",        :null => false
@@ -167,7 +252,14 @@ ActiveRecord::Schema.define(:version => 20111203201549) do
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
-  create_table "speaks", :force => true do |t|
+  create_table "specialist_addresses", :force => true do |t|
+    t.integer  "specialist_id"
+    t.integer  "address_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "specialist_speaks", :force => true do |t|
     t.integer  "specialist_id"
     t.integer  "language_id"
     t.datetime "created_at"
