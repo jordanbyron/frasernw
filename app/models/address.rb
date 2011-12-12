4d0f8706ea67f68bebda30e6c26a65f7c3e424b8
Russell Warneboldt <warneboldt@gmail.com>
2011-12-12 13:33:24 -0800

    specialist display quickly updated to show captured form information

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,8 @@
 class Address < ActiveRecord::Base
   has_paper_trail
   belongs_to :hospital
+  
+  def empty?
+    address1.blank? and address2.blank? and city.blank? and province.blank? and postalcode.blank? and phone1.blank? and fax.blank? and hospital_id.blank?
+  end
 end
