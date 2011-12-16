4a9aac5e6438d2c0fc40dd4351816fafbbbfdd7c
Russell Warneboldt <warneboldt@gmail.com>
2011-12-16 13:55:15 -0800

    Clinics show procedure scope when picking

--- app/helpers/clinics_helper.rb
+++ app/helpers/clinics_helper.rb
@@ -1,2 +1,15 @@
 module ClinicsHelper
+  
+  def ancestry_options(items, &block)
+    return ancestry_options(items){ |i| "#{'-' * i.depth} #{i.name}" } unless block_given?
+    
+    result = []
+    items.map do |item, sub_items|
+      result << [yield(item), item.id]
+      #this is a recursive call:
+      result += ancestry_options(sub_items, &block)
+    end
+    result
+  end
+  
 end
--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -11,8 +11,6 @@ class Procedure < ActiveRecord::Base
   
   belongs_to :specialization
 
-  default_scope :order => 'name ASC'
-
   validates_presence_of :specialization_id, :on => :save, :message => "can't be blank"
   validates_presence_of :name, :on => :save, :message => "can't be blank"
   
