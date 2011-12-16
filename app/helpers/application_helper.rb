34ae3f1d096802ae5c9f526c8f4bc5b65af96874
Russell Warneboldt <warneboldt@gmail.com>
2011-12-16 13:57:56 -0800

    Specialist show procedure ancestry when editing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -77,4 +77,16 @@ module ApplicationHelper
     output.gsub(/\,\ $/,'')
   end
   
+  def ancestry_options(items, &block)
+    return ancestry_options(items){ |i| "#{'-' * i.depth} #{i.name}" } unless block_given?
+    
+    result = []
+    items.map do |item, sub_items|
+      result << [yield(item), item.id]
+      #this is a recursive call:
+      result += ancestry_options(sub_items, &block)
+    end
+    result
+  end
+  
 end
