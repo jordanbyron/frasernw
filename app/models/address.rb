ae46283b0482fb77a58099c5bd1e8f3b74b23476
Russell Warneboldt <warneboldt@gmail.com>
2011-12-18 19:03:01 -0800

    updating address class for new city / province format

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,7 +3,28 @@ class Address < ActiveRecord::Base
   belongs_to :hospital
   belongs_to :city
   
+  def address
+    output = ""
+    if address2.present? and address1.present?
+      output = "#{address2} - #{address1}, "
+    elsif address2.present?
+      output = "#{address2}, "
+    elsif address1.present?
+      output = "#{address1}, "
+    end
+    
+    if city
+      output += "#{city}, #{city.province}, "
+    end
+    
+    if postalcode.present?
+      output = "#{postalcode}, "
+    end
+    
+    return output[0..-3]
+  end
+  
   def empty?
-    address1.blank? and address2.blank? and city.blank? and postalcode.blank? and phone1.blank? and fax.blank? and hospital_id.blank?
+    address1.blank? and address2.blank? and (not city) and postalcode.blank? and phone1.blank? and fax.blank? and hospital_id.blank?
   end
 end
