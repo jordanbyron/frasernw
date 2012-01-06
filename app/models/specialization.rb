1773fc6ae2572977e48b957b2ce3495b431d58a7
Russell Warneboldt <warneboldt@gmail.com>
2012-01-06 14:41:27 -0800

    Specializations recognize clinics can belong to multiple specializations

--- app/models/specialization.rb
+++ app/models/specialization.rb
@@ -5,7 +5,8 @@ class Specialization < ActiveRecord::Base
   has_many :specialist_specializations, :dependent => :destroy
   has_many :specialists, :through => :specialist_specializations
   
-  has_many :clinics
+  has_many :clinic_specializations, :dependent => :destroy
+  has_many :clinics, :through => :clinic_specializations
   
   has_many :procedure_specializations, :dependent => :destroy, :conditions => { "mapped" => true }#, :finder_sql => 'SELECT ps.* FROM procedure_specializations ps JOIN procedures p ON ps.procedure_id = p.id WHERE ps.specialization_id = #{id} ORDER BY p.name ASC'
   has_many :procedures, :through => :procedure_specializations, :uniq => true, :conditions => { "procedure_specializations.mapped" => true }, :order => 'name ASC'
