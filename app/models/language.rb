3cd631eddf31b194fc1d49a33ccac893bb7cfa08
Russell Warneboldt <warneboldt@gmail.com>
2012-01-07 16:00:54 -0800

    fixing languages model to point at right table

--- app/models/language.rb
+++ app/models/language.rb
@@ -2,8 +2,8 @@ class Language < ActiveRecord::Base
   attr_accessible :name
   has_paper_trail meta: { to_review: false }
   
-  has_many :speaks, :dependent => :destroy
-  has_many :specialists, :through => :speaks
+  has_many :specialist_speaks, :dependent => :destroy
+  has_many :specialists, :through => :specialist_speaks
   
   has_many :clinic_speaks, :dependent => :destroy
   has_many :clinics, :through => :clinic_speaks
