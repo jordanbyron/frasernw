2ceb51d328bfe6ab6cbca766cc1df6b136ba76ef
Russell Warneboldt <warneboldt@gmail.com>
2012-01-10 09:45:16 -0800

    Procedures arrange properly off a specialization

--- app/models/specialization.rb
+++ app/models/specialization.rb
@@ -8,10 +8,14 @@ class Specialization < ActiveRecord::Base
   has_many :clinic_specializations, :dependent => :destroy
   has_many :clinics, :through => :clinic_specializations
   
-  has_many :procedure_specializations, :dependent => :destroy, :conditions => { "mapped" => true }, :finder_sql => proc { "SELECT DISTINCT ps.* FROM procedure_specializations ps JOIN procedures p ON ps.procedure_id = p.id WHERE ps.specialization_id = #{self.id} ORDER BY p.name ASC" }
+  has_many :procedure_specializations, :dependent => :destroy, :finder_sql => proc { "SELECT DISTINCT ps.* FROM procedure_specializations ps JOIN procedures p ON ps.procedure_id = p.id WHERE ps.specialization_id = #{self.id} AND ps.mapped = 't' ORDER BY p.name ASC" }
   has_many :procedures, :through => :procedure_specializations, :uniq => true, :conditions => { "procedure_specializations.mapped" => true }, :order => 'name ASC'
   
   default_scope order('name')
+  
+  def procedure_specializations_arranged
+    return procedure_specializations.arrange(:joins => "JOIN procedures ON procedure_specializations.procedure_id = procedures.id", :order => "procedures.name")
+  end
 
 end
 
