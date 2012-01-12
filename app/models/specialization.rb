73d4883dee1b93d28543aae6ac2d5b3dbdea8bf8
Russell Warneboldt <warneboldt@gmail.com>
2012-01-12 13:49:33 -0800

    Fixing specializations SQL statement

--- app/models/specialization.rb
+++ app/models/specialization.rb
@@ -8,7 +8,7 @@ class Specialization < ActiveRecord::Base
   has_many :clinic_specializations, :dependent => :destroy
   has_many :clinics, :through => :clinic_specializations
   
-  has_many :procedure_specializations, :dependent => :destroy, :finder_sql => proc { "SELECT DISTINCT ps.* FROM procedure_specializations ps JOIN procedures p ON ps.procedure_id = p.id WHERE ps.specialization_id = #{self.id} AND ps.mapped = 't' ORDER BY p.name ASC" }
+  has_many :procedure_specializations, :dependent => :destroy, :finder_sql => proc { "SELECT DISTINCT ps.*, p.name FROM procedure_specializations ps JOIN procedures p ON ps.procedure_id = p.id WHERE ps.specialization_id = #{self.id} AND ps.mapped = 't' ORDER BY p.name ASC" }
   has_many :procedures, :through => :procedure_specializations, :uniq => true, :conditions => { "procedure_specializations.mapped" => true }, :order => 'name ASC'
   
   default_scope order('name')
