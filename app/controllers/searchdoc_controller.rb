a19aa0759b4235d808545c1bb3fbbe2b8cc40029
Russell Warneboldt <warneboldt@gmail.com>
2012-01-13 15:23:43 -0800

    Navigation works with nested procedures and multiple specialists/clinics per specialization

--- app/controllers/searchdoc_controller.rb
+++ app/controllers/searchdoc_controller.rb
@@ -1,10 +1,9 @@
 class SearchdocController < ApplicationController
 skip_authorization_check
+  
 def navigation
-  @specializations = Specialization.all
-  @specialists = Specialist.all
-  @clinics = Clinic.all
 end
+  
 def tree
 #RPW TODO optimize by this?
     #@specializations = Specialization.find(:all, :include => [ :specialists, :clinics, { :procedure_specializations => :procedure } ] )
