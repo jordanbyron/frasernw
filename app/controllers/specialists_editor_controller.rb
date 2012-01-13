b6c83508c912f3aca1edab3b5563a5ddefdf633f
Russell Warneboldt <warneboldt@gmail.com>
2012-01-12 16:05:45 -0800

    fixing review queue

--- app/controllers/specialists_editor_controller.rb
+++ app/controllers/specialists_editor_controller.rb
@@ -1,4 +1,5 @@
 class SpecialistsEditorController < ApplicationController
+  include ApplicationHelper
   skip_before_filter :login_required
   skip_authorization_check
   before_filter { |controller|  controller.send(:specialist_token_required, params[:token], params[:id]) }
@@ -6,26 +7,44 @@ class SpecialistsEditorController < ApplicationController
   def edit
     @token      = params[:token]
     @specialist = Specialist.find(params[:id])
-    @specialist.capacities.build if @specialist.capacities.count == 0
-    @specialization = @specialist.specialization
-    @specialization_clinics = @specialization.clinics.collect { |clinic| [clinic.name, clinic.id] }.sort
-    @specialist_clinics = @specialist.clinics.collect {|c| c.id}
+    if @specialist.capacities.count == 0
+      @specialist.capacities.build
+    end
+    @specializations_clinics = []
+    @specialist.specializations.each { |s| 
+      @specializations_clinics += s.clinics.collect { |c| [c.name, c.id] }
+    }
+    @specializations_clinics.sort!
+    @specializations_procedures = []
+    @specialist.specializations.each { |s| 
+      @specializations_procedures << [ "----- #{s.name} -----", nil ] if @specialist.specializations.count > 1
+      @specializations_procedures += ancestry_options( s.procedure_specializations_arranged )
+    }
     @view = @specialist.views.build(:notes => request.remote_ip)
     @view.save
     render :template => 'specialists/edit'
   end
 
   def update
-    params[:specialist][:procedure_ids] ||= []
     @specialist = Specialist.find(params[:id])
     if @specialist.update_attributes!(params[:specialist])
       redirect_to specialist_self_edit_path(@specialist), :notice => "You have successfully updated the information for #{@specialist.name}."
     else
       @token = params[:token]
-      @specialist.capacities.build if @specialist.capacities.count == 0
-      @specialization = @specialist.specialization
-      @specialization_clinics = @specialization.clinics.collect { |clinic| [clinic.name, clinic.id] }.sort
-      @specialist_clinics = @specialist.clinics.collect {|c| c.id}
+      @specialist = Specialist.find(params[:id])
+      if @specialist.capacities.count == 0
+        @specialist.capacities.build
+      end
+      @specializations_clinics = []
+      @specialist.specializations.each { |s| 
+        @specializations_clinics += s.clinics.collect { |c| [c.name, c.id] }
+      }
+      @specializations_clinics.sort!
+      @specializations_procedures = []
+      @specialist.specializations.each { |s| 
+        @specializations_procedures << [ "----- #{s.name} -----", nil ] if @specialist.specializations.count > 1
+        @specializations_procedures += ancestry_options( s.procedure_specializations_arranged )
+      }
       @view = @specialist.views.build(:notes => request.remote_ip)
       @view.save
       render :template => 'specialists/edit'
