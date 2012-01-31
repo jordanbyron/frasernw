b7b9757f281e4b126886790e4533238fa032d913
Russell Warneboldt <warneboldt@gmail.com>
2012-01-31 11:02:02 -0800

    new clinic view looks like new specialist view

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -42,7 +42,11 @@ class Clinic < ActiveRecord::Base
   }
   
   def status
-    Clinic::STATUS_HASH[status_mask]
+    if ((status_mask != 1) and (status_mask != 2))
+      "It is unknown if this clinic is accepting new patients (they didn't answer)"
+    else
+      Clinic::STATUS_HASH[status_mask]
+    end
   end
   
   def status_class
@@ -105,5 +109,91 @@ class Clinic < ActiveRecord::Base
   
   def patient_can_book
     Specialist::BOOLEAN_HASH[patient_can_book_mask]
+  end
+  
+  def accepts_referrals_via
+    if referral_phone and referral_fax and referral_other_details.presence
+      return "phone, fax, or " + referral_other_details
+    elsif referral_phone and referral_fax
+      return "phone or fax"
+    elsif referral_phone
+      if referral_other_details.presence
+        return "phone or " + referral_other_details
+      else
+        return "phone"
+      end
+    elsif referral_fax
+      if referral_other_details.presence
+        return "fax or " + referral_other_details
+      else
+        return "fax"
+      end
+    elsif referral_other_details.presence
+      return referral_other_details
+    else
+      return "unspecified"
+    end
+  end
+  
+  def responds_via
+    if (not respond_by_phone) and (not respond_by_fax) and (not respond_by_mail) and (not respond_to_patient)
+      return ""
+    elsif (not respond_by_phone) and (not respond_by_fax) and (not respond_by_mail) and respond_to_patient
+      return "directly to patient"
+    else
+      if respond_by_phone and respond_by_fax and respond_by_mail
+        output = "phone, fax, or mail to referring office"
+      elsif respond_by_phone and respond_by_fax and (not respond_by_mail)
+        output = "phone or fax to referring office"
+      elsif respond_by_phone and (not respond_by_fax) and respond_by_mail
+        output = "phone or mail to referring office"
+      elsif respond_by_phone and (not respond_by_fax) and (not respond_by_mail)
+        output = "phone to referring office"
+      elsif (not respond_by_phone) and respond_by_fax and respond_by_mail
+        output = "fax or mail to referring office"
+      elsif (not respond_by_phone) and respond_by_fax and (not respond_by_mail)
+        output = "fax to referring office"
+      else # must be (not respond_by_phone) and (not respond_by_fax) and respond_by_mail
+        output = "mail to referring office"
+      end
+      
+      if respond_to_patient
+        return output + ", and directly to patient"
+      else
+        return output
+      end
+    end
+  end
+  
+  def urgent_referrals_via
+    if urgent_phone and urgent_fax and urgent_other_details.presence
+      return "phone, fax, or " + urgent_other_details
+    elsif urgent_phone and urgent_fax
+      return "phone or fax"
+    elsif urgent_phone
+      output = "phone"
+      if urgent_other_details.presence
+        return output + " or " + urgent_other_details
+      end
+    elsif urgent_fax
+      output = "fax"
+      if urgent_other_details.presence
+        return output + " or " + urgent_other_details
+      end
+    elsif urgent_other_details.presence
+      return referral_other_details
+    else
+      return ""
+    end
+  end
+
+  def token
+    if self.saved_token
+      return self.saved_token
+    else
+      self.saved_token = SecureRandom.hex(16)
+      self.save
+      return self.saved_token
+    end
   end
 end
