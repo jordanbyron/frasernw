f84c5ec048e8f38b6c4ae78492c5ace33a4226a9
Russell Warneboldt <warneboldt@gmail.com>
2012-02-01 14:53:01 -0800

    Adding clinic and specialist_office associations to hospitals (based off address taggings)

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -5,6 +5,10 @@ class Hospital < ActiveRecord::Base
   has_many :privileges, :dependent => :destroy
   has_many :specialists, :through => :privileges
   
+  has_many :clinics, :finder_sql => proc { "SELECT DISTINCT c.* FROM clinics c JOIN clinic_addresses ca ON c.id = ca.clinic_id JOIN addresses a ON ca.address_id = a.id WHERE a.hospital_id = #{self.id} ORDER BY c.name ASC" }
+  
+  has_many :specialist_offices, :finder_sql => proc { "SELECT DISTINCT s.* FROM specialists s JOIN specialist_addresses sa ON s.id = sa.specialist_id JOIN addresses a ON sa.address_id = a.id WHERE a.hospital_id = #{self.id} ORDER BY s.lastname ASC, s.firstname ASC" }, :class_name => "Specialist"
+  
   # hospitals can have more than one address
   MAX_ADDRESSES = 2
   has_many :hospital_addresses
