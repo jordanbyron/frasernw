8065e5bf992e0d827832214518f77c5867e2eb09
Russell Warneboldt <warneboldt@gmail.com>
2012-02-03 08:45:04 -0800

    navigation resizes with transition

--- app/assets/javascripts/frasernw-nav.js
+++ app/assets/javascripts/frasernw-nav.js
@@ -2,7 +2,7 @@
 
   "use strict"
 
-  var nav_transition = function ( target, method, startEvent, completeEvent ) 
+  var nav_transition = function ( target, resize, method, startEvent, completeEvent ) 
   {
     var that = $(target)
     , complete = function () {
@@ -13,20 +13,22 @@
       .trigger(startEvent)
       [method]('pushed')
     
+    $('#left-nav').height( $(resize)[0].scrollHeight )
+    
     $.support.transition && $(target).hasClass('nav_panel') ?
       $(target).one($.support.transition.end, complete) :
       complete()
   }
 
-  var nav_push = function ( target ) {
+  var nav_push = function ( target, resize ) {
     $(target).each(function () {
-      nav_transition(target, 'addClass', 'show', 'shown')
+      nav_transition(target, resize, 'addClass', 'show', 'push')
     })
   }
 
-  var nav_pop = function ( target ) {
+  var nav_pop = function ( target, resize ) {
     $(target).each(function () {
-      nav_transition(target, 'removeClass', 'hide', 'hidden')
+      nav_transition(target, resize, 'removeClass', 'hide', 'pop')
     })
   }
 
@@ -34,13 +36,15 @@
     $('body').on('click.nav_panel.data-api', '[data-toggle=push]', function ( e ) {
       var $this = $(this)
         , target = $this.attr('data-target') || e.preventDefault() || $this.attr('href')
-      nav_push(target)
+        , resize = target
+      nav_push(target, resize)
     })
     
     $('body').on('click.nav_panel.data-api', '[data-toggle=pop]', function ( e ) {
       var $this = $(this)
         , target = $this.attr('data-target') || e.preventDefault() || $this.attr('href')
-      nav_pop(target)
+        , resize = $this.attr('data-parent') || e.preventDefault()
+      nav_pop(target, resize)
     })
   })
 
