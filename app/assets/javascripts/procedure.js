d5fcb5e1db90f3ad42021087948be15a07e06181
Russell Warneboldt <warneboldt@gmail.com>
2012-02-10 13:17:37 -0800

    Styling procedures admin

--- app/assets/javascripts/procedure.js
+++ app/assets/javascripts/procedure.js
@@ -1,14 +1,17 @@
 var hideShowParents = function() 
 {
+  var currentId = $(this).attr('id');
+  var select = "#" + currentId.replace("check","select");
+  
   if ( $(this).is(":checked") )
   {
     //we have indicated that this area of practice belongs to this specializatios, so let us pick a parent
-    $(this).siblings(".parent").show();
+    $(select).show();
   }
   else
   {
     //we have indicated that this area of practice does not belong to this specialization, so hide parents 
-    $(this).siblings(".parent").hide();
+    $(select).hide();
   }
 }
 
