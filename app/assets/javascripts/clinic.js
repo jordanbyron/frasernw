b21b3119124daaddf3c7c746a541e447081affaf
Russell Warneboldt <warneboldt@gmail.com>
2012-02-10 11:52:16 -0800

    Clinic form

--- app/assets/javascripts/clinic.js
+++ app/assets/javascripts/clinic.js
@@ -1,16 +1,24 @@
 var hideShowPhysicians = function() 
 {
+  var currentId = $(this).attr('id');
+  
+  var specialists = "#" + currentId.replace("is_specialist","specialist_id");
+  var firstname = "#" + currentId.replace("is_specialist","freeform_firstname");
+  var lastname = "#" + currentId.replace("is_specialist","freeform_lastname");
+  
   if ( $(this).is(":checked") )
   {
     //we have indicated a specialist
-    $(this).siblings(".specialist_list").show();
-    $(this).siblings(".freeform").hide();
+    $(specialists).show();
+    $(firstname).hide();
+    $(lastname).hide();
   }
   else
   {
     //we have indicated a freeform answer
-    $(this).siblings(".specialist_list").hide();
-    $(this).siblings(".freeform").show();
+    $(specialists).hide();
+    $(firstname).show();
+    $(lastname).show();
   }
 }
 
