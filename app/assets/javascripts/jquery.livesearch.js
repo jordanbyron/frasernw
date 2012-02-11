7b0063120132ab5e82648ead67198b306622aec5
Russell Warneboldt <warneboldt@gmail.com>
2012-02-11 13:47:35 -0800

    Very rough first pass at a live search implementation, based off of search branch

---
+++ app/assets/javascripts/jquery.livesearch.js
@@ -0,0 +1,206 @@
+/*
+ * search_data needs to be an array of search data entries, where each entry is an array of info. e.g. [["name 1", "url 1"], ["name 2", "url 2"]].
+ * scores_matches_fnc(search_data_entry, term) needs to be a function which takes in a search data entry and search term, and returns an array of two arrays, [[scores],[matches]] for each element in the search_data_entry which you want to search against. For scores, higher is a better match, with zero indicating no match. Note that the String.score_matches function is useful here.
+ * format_search_result(total_score, scores, matches, search_data_entry) needs to be a function which takes in a total score, array of scores and matches (as returned by your scores_matches_fnc), and the original search_data_entry, and returns a formatted list element (e.g. <li>something</li>).
+*/
+
+jQuery.fn.livesearch = function(list, search_data, scores_matches_fnc, format_search_result_fnc)
+{
+	list = jQuery(list);
+    search_data = jQuery(search_data);
+        
+    this
+    .keyup(filter).keyup()
+    .parents('form').submit(function()
+                            {
+                                return false;
+                            });
+    
+	return this;
+    
+	function filter()
+    {
+		var term = jQuery.trim( jQuery(this).val().toLowerCase() )
+        var results = [];
+		
+		if ( !term ) 
+        {
+			list.hide();
+		} 
+        else 
+        {
+            list.empty();
+			list.show();
+            
+			search_data.each(   function()
+                                {
+                                    var total_score = 0;
+                                    var scores_matches = scores_matches_fnc(this, term);
+                                    var scores = scores_matches[0];
+                                    var matches = scores_matches[1];
+                                    for ( var i = 0; i < scores.length; ++i )
+                                    {
+                                        total_score += scores[i];
+                                    }
+                             
+                                    if (total_score > 0) 
+                                    { 
+                                        results.push([total_score, scores, matches, this]); 
+                                    }
+                                });
+            
+			jQuery.each(results.sort(function(a, b){return b[0] - a[0];}), function()
+                        {
+                            list.append(format_search_result_fnc(this[0], this[1], this[2], this[3]));
+                        });
+		}
+	}
+};
+
+ /*!
+ * score_matches, built off of string_score
+ * 
+ * string_score.js: String Scoring Algorithm 0.1.10 
+ *
+ * http://joshaven.com/string_score
+ * https://github.com/joshaven/string_score
+ *
+ * Copyright (C) 2009-2011 Joshaven Potter <yourtech@gmail.com>
+ * Special thanks to all of the contributors listed here https://github.com/joshaven/string_score
+ * MIT license: http://www.opensource.org/licenses/mit-license.php
+ *
+ * Date: Tue Mar 1 2011
+ */
+
+/**
+ * Scores a string against another string.
+ * Modified to also return array of matching characters, for highlighting purposes.
+ *  'Hello World'.score('he');     //=> [0.5931818181818181, [0,1]]
+ *  'Hello World'.score('Hello');  //=> [0.7318181818181818, [0,1,2,3,4]]
+ */
+
+
+String.prototype.score_matches = function(abbreviation, fuzziness) 
+{
+    var matches = new Array();
+    
+    // If the string is equal to the abbreviation, perfect match.
+    if (this == abbreviation) 
+    {
+        for ( var i = 0; i < this.length; ++i )
+        {
+            matches[i] = i;
+        }
+        return [1,matches];
+    }
+    //if it's not a perfect match and is empty return 0
+    if(abbreviation == "") {return [0,[]];}
+    
+    var total_character_score = 0,
+    string = this,
+    string_length = string.length,
+    abbreviation_length = abbreviation.length,
+    start_of_string_bonus,
+    abbreviation_score,
+    cur_start = 0,
+    fuzzies = 1,
+    final_score
+    
+    // Walk through abbreviation and add up scores.
+    for (var i = 0,
+         character_score/* = 0*/,
+         index_in_string/* = 0*/,
+         c/* = ''*/,
+         index_c_lowercase/* = 0*/,
+         index_c_uppercase/* = 0*/,
+         min_index/* = 0*/;
+         i < abbreviation_length;
+         ++i) 
+    {
+        
+        // Find the first case-insensitive match of a character.
+        c = abbreviation.charAt(i);
+        
+        index_c_lowercase = string.indexOf(c.toLowerCase());
+        index_c_uppercase = string.indexOf(c.toUpperCase());
+        min_index = Math.min(index_c_lowercase, index_c_uppercase);
+        index_in_string = (min_index > -1) ? min_index : Math.max(index_c_lowercase, index_c_uppercase);
+        
+        if (index_in_string === -1) 
+        { 
+            if (fuzziness) 
+            {
+                fuzzies += 1-fuzziness;
+                continue;
+            } 
+            else 
+            {
+                return [0,[]];
+            }
+        } 
+        else 
+        {
+            matches.push(cur_start + index_in_string);
+            character_score = 0.1;
+        }
+        
+        // Set base score for matching 'c'.
+        
+        // Same case bonus.
+        if (string[index_in_string] === c) 
+        { 
+            character_score += 0.1; 
+        }
+        
+        // Consecutive letter & start-of-string Bonus
+        if (index_in_string === 0) 
+        {
+            // Increase the score when matching first character of the remainder of the string
+            character_score += 0.6;
+            if (i === 0) 
+            {
+                // If match is the first character of the string
+                // & the first character of abbreviation, add a
+                // start-of-string match bonus.
+                start_of_string_bonus = 1 //true;
+            }
+        }
+        else 
+        {
+            // Acronym Bonus
+            // Weighing Logic: Typing the first character of an acronym is as if you
+            // preceded it with two perfect character matches.
+            if (string.charAt(index_in_string - 1) === ' ') 
+            {
+                character_score += 0.8; // * Math.min(index_in_string, 5); // Cap bonus at 0.4 * 5
+            }
+        }
+        
+        // Left trim the already matched part of the string
+        // (forces sequential matching).
+        string = string.substring(index_in_string + 1, string_length);
+        cur_start += index_in_string + 1;
+        
+        total_character_score += character_score;
+    } // end of for loop
+    
+    // Uncomment to weigh smaller words higher.
+    // return total_character_score / string_length;
+    
+    abbreviation_score = total_character_score / abbreviation_length;
+    //percentage_of_matched_string = abbreviation_length / string_length;
+    //word_score = abbreviation_score * percentage_of_matched_string;
+    
+    // Reduce penalty for longer strings.
+    //final_score = (word_score + abbreviation_score) / 2;
+    final_score = ((abbreviation_score * (abbreviation_length / string_length)) + abbreviation_score) / 2;
+    
+    final_score = final_score / fuzzies;
+    
+    if (start_of_string_bonus && (final_score + 0.15 < 1)) 
+    {
+        final_score += 0.15;
+    }
+    
+    return [final_score, matches];
+};
