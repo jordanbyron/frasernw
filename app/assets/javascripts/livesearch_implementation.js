9515d5500c6a6805289ae59da57a34ee5762c96b
Russell Warneboldt <warneboldt@gmail.com>
2012-02-11 22:50:13 -0800

    Search styling and content (specialists, clinics, hospitals, specializations, and procedures)

--- app/assets/javascripts/livesearch_implementation.js
+++ app/assets/javascripts/livesearch_implementation.js
@@ -2,8 +2,7 @@
 function pathways_scorer(data_entry, term, fuzziness) 
 {
   return { 
-    name: data_entry.name.score_matches(term, fuzziness), 
-    specialty: data_entry.specialty.score_matches(term, fuzziness) 
+    name: data_entry.name.score_matches(term, fuzziness)
   };
 }
 
@@ -11,8 +10,10 @@ function pathways_formatter(total_score, scores_matches, data_entry)
 {
   var result = "<li><a href=\"#\" onclick=\"ajaxto('" + data_entry.url + "'); $('#search_results').removeClass('show');\">"
   
-  result += "<span class='name'>" + livesearch_highlighter( data_entry.name, scores_matches.name.matches ) + "</span>"
-  result += "<span class='specialties'>" + livesearch_highlighter( data_entry.specialty, scores_matches.specialty.matches ) + "</span>"
+  result += "<div class='icon spanhalf'><i class='icon-" + data_entry.icon + "'></i></div>"
+  result += "<div class='name span2'>" + livesearch_highlighter( data_entry.name, scores_matches.name.matches ) + "</div>"
+  result += "<div class='specialties span2 offsethalf status_" + data_entry.status + "'>" + data_entry.specialty + "</div>"
+  result += "<div class='city span2'>" + data_entry.city + "</div>"
   
   result += "</a></li>";
   
