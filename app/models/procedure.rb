fb93a577b022d2fd4861b0454983f5de2c26168c
Russell Warneboldt <warneboldt@gmail.com>
2012-02-15 10:29:07 -0800

    Adding missing method for procedure specializations_including_in_progress

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -13,6 +13,7 @@ class Procedure < ActiveRecord::Base
   has_many :all_procedure_specializations, :dependent => :destroy, :class_name => "ProcedureSpecialization"
   has_many :procedure_specializations, :dependent => :destroy, :conditions => { "mapped" => true }
   has_many :specializations, :through => :procedure_specializations, :uniq => true, :conditions => { "procedure_specializations.mapped" => true, "in_progress" => false }, :order => 'name ASC'
+  has_many :specializations_including_in_progress, :through => :procedure_specializations, :uniq => true, :conditions => { "procedure_specializations.mapped" => true }, :order => 'name ASC', :source => :specialization, :class_name => "Specialization"
   
   accepts_nested_attributes_for :all_procedure_specializations, :allow_destroy => true
 
