c400104ef1ae4e06d81fb2ebc12e88626dfb462c
Russell Warneboldt <warneboldt@gmail.com>
2012-02-22 18:01:05 -0800

    Review items are created by the specialist_editor

--- app/controllers/specialists_editor_controller.rb
+++ app/controllers/specialists_editor_controller.rb
@@ -27,28 +27,15 @@ class SpecialistsEditorController < ApplicationController
 
   def update
     @specialist = Specialist.find(params[:id])
-    if @specialist.update_attributes!(params[:specialist])
-      redirect_to specialist_self_edit_path(@specialist), :notice => "You have successfully updated the information for #{@specialist.name}."
-    else
-      @token = params[:token]
-      @specialist = Specialist.find(params[:id])
-      if @specialist.capacities.count == 0
-        @specialist.capacities.build
-      end
-      @specializations_clinics = []
-      @specialist.specializations_including_in_progress.each { |s| 
-        @specializations_clinics += s.clinics.collect { |c| [c.name, c.id] }
-      }
-      @specializations_clinics.sort!
-      @specializations_procedures = []
-      @specialist.specializations_including_in_progress.each { |s| 
-        @specializations_procedures << [ "----- #{s.name} -----", nil ] if @specialist.specializations_including_in_progress.count > 1
-        @specializations_procedures += ancestry_options( s.procedure_specializations_arranged )
-      }
-      @view = @specialist.views.build(:notes => request.remote_ip)
-      @view.save
-      render :template => 'specialists/edit'
-    end
+    
+    review_item = ReviewItem.new
+    review_item.item_type = "Specialist"
+    review_item.item_id = @specialist.id
+    review_item.object = ActiveSupport::JSON::encode(params[:specialist])
+    review_item.whodunnit = "Unknown"
+    review_item.save
+    
+    redirect_to specialist_self_edit_path(@specialist), :notice => "You have successfully updated the information for #{@specialist.name}."
   end
 
 end
