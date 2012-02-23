ab8549400d505f9f5ba769347ba412bc06920ca0
Russell Warneboldt <warneboldt@gmail.com>
2012-02-22 17:18:26 -0800

    Cleaning up review items controller

--- app/controllers/review_items_controller.rb
+++ app/controllers/review_items_controller.rb
@@ -1,83 +1,48 @@
 class ReviewItemsController < ApplicationController
-  # GET /review_items
-  # GET /review_items.json
+  load_and_authorize_resource
+  
   def index
     @review_items = ReviewItem.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @review_items }
-    end
+    render :layout => 'ajax' if request.headers['X-PJAX']
   end
-
-  # GET /review_items/1
-  # GET /review_items/1.json
+  
   def show
     @review_item = ReviewItem.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @review_item }
-    end
+    render :layout => 'ajax' if request.headers['X-PJAX']
   end
-
-  # GET /review_items/new
-  # GET /review_items/new.json
+  
   def new
     @review_item = ReviewItem.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @review_item }
-    end
+    @review_item.addresses.build
+    render :layout => 'ajax' if request.headers['X-PJAX']
   end
-
-  # GET /review_items/1/edit
-  def edit
-    @review_item = ReviewItem.find(params[:id])
-  end
-
-  # POST /review_items
-  # POST /review_items.json
+  
   def create
     @review_item = ReviewItem.new(params[:review_item])
-
-    respond_to do |format|
-      if @review_item.save
-        format.html { redirect_to @review_item, notice: 'Review item was successfully created.' }
-        format.json { render json: @review_item, status: :created, location: @review_item }
+    if @review_item.save
+      redirect_to @review_item, :notice => "Successfully created review_item."
       else
-        format.html { render action: "new" }
-        format.json { render json: @review_item.errors, status: :unprocessable_entity }
-      end
+      render :action => 'new'
     end
   end
-
-  # PUT /review_items/1
-  # PUT /review_items/1.json
+  
+  def edit
+    @review_item = ReviewItem.find(params[:id])
+    render :layout => 'ajax' if request.headers['X-PJAX']
+  end
+  
   def update
     @review_item = ReviewItem.find(params[:id])
-
-    respond_to do |format|
-      if @review_item.update_attributes(params[:review_item])
-        format.html { redirect_to @review_item, notice: 'Review item was successfully updated.' }
-        format.json { head :ok }
+    if @review_item.update_attributes(params[:review_item])
+      redirect_to @review_item, :notice  => "Successfully updated review_item."
       else
-        format.html { render action: "edit" }
-        format.json { render json: @review_item.errors, status: :unprocessable_entity }
-      end
+      render :action => 'edit'
     end
   end
-
-  # DELETE /review_items/1
-  # DELETE /review_items/1.json
+  
   def destroy
     @review_item = ReviewItem.find(params[:id])
     @review_item.destroy
-
-    respond_to do |format|
-      format.html { redirect_to review_items_url }
-      format.json { head :ok }
-    end
+    redirect_to review_items_url, :notice => "Successfully deleted review_item."
   end
 end
