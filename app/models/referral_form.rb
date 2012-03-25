540a1c74489e9ba6aff0ec3afa68f41198dc809d
Russell Warneboldt <warneboldt@gmail.com>
2012-03-25 09:30:11 -0700

    referral forms save to Amazon S3

--- app/models/referral_form.rb
+++ app/models/referral_form.rb
@@ -1,7 +1,13 @@
 class ReferralForm < ActiveRecord::Base
   belongs_to :referrable, :polymorphic => true
   
-  has_attached_file :form
+  has_attached_file :form,
+    :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :s3_credentials => {
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
   
   has_paper_trail
 end
