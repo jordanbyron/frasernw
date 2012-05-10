96c11c9e5e48d6fce2675565149b705cad9d1b88
Russell Warneboldt <warneboldt@gmail.com>
2012-05-10 12:25:01 -0700

    Languages and procedures on index cards are links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -53,7 +53,7 @@ module ApplicationHelper
     items.each do |item|
       count += 1
       result += "<li>"
-      result += item[:parent].procedure.name
+      result += "<a class='ajax' href='#{procedure_path(item[:parent].procedure)}'>#{item[:parent].procedure.name}</a>"
       investigation = item[:parent].investigation(root)
       if investigation and investigation.length > 0
         result += ": #{investigation}"
