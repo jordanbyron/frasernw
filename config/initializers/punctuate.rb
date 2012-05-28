81082699eb292024417f3bf6ece634888e545b8e
Russell Warneboldt <warneboldt@gmail.com>
2012-05-28 14:25:04 -0700

    Fixing double period

--- config/initializers/punctuate.rb
+++ config/initializers/punctuate.rb
@@ -1,10 +1,12 @@
 class String
   
   def capitalize_first_letter
+    self.strip!
     self.slice(0,1).capitalize + self.slice(1..-1)
   end
   
   def end_with_period
+    self.strip!
     if self[-1,1] != '.'
       self + "."
     else
