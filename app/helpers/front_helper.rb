2eea5d67e26c9e5ee3bf46488533c51a92c07ed3
Russell Warneboldt <warneboldt@gmail.com>
2012-05-31 19:09:57 -0700

    Activity feed is substantially improved. Multiple entries about the same specialist / clinic are much better handled, and new events are in the feed.

--- app/helpers/front_helper.rb
+++ app/helpers/front_helper.rb
@@ -1,91 +1,156 @@
 module FrontHelper
   
   def latest_events(max_events)
-    events = []
-    Version.order("id desc").each do |version|
-      event = interesting_event(version)
-      events << event if event.present?
-      return events if events.length >= max_events
-    end
-    return events
-  end
-  
-  def interesting_event(version)
     
-    return false if version.item.blank?
-      
-    if version.item_type == "Specialist"
-      
-      specialist = version.item
-      return false if specialist.blank?
-      
-      if version.event == "create" && specialist.accepting_new_patients?
+    events = {}
         
-        #new specialist that is accepting patients
-        
-        if specialist.city.present? 
-          return "#{link_to specialist.name, specialist_path(specialist), :class => 'ajax'} (#{specialist.specializations.map{ |s| s.name }.to_sentence}) is accepting patients in #{specialist.city}. #{version.id}".html_safe
-        else 
-          return "#{link_to specialist.name, specialist_path(specialist), :class => 'ajax'} (#{specialist.specializations.map{ |s| s.name }.to_sentence}) is accepting patients. #{version.id}".html_safe
-        end
-      
-      elsif version.event == "update"
-      
-        if specialist.retired?
-          
-          return false if version.previous.blank? || version.previous.reify.blank?
-          return false if version.previous.reify.retired? #retired status hasn't changed
-          
-          #newly retired
-          
-          return "#{link_to specialist.name, specialist_path(specialist), :class => 'ajax'} (#{specialist.specializations.map{ |s| s.name }.to_sentence}) has retired. #{version.id}".html_safe
-          
-        elsif specialist.retiring?
-          
-          return false if version.previous.blank? || version.previous.reify.blank?
-          return false if version.previous.reify.retiring? #retiring status hasn't changed
-          
-          return "#{link_to specialist.name, specialist_path(specialist), :class => 'ajax'} (#{specialist.specializations.map{ |s| s.name }.to_sentence}) is retiring on #{specialist.unavailable_from.strftime('%B %d, %Y')}. #{version.id}".html_safe          
-          
-        end
-      
-      end
-      
-    elsif version.item_type == "Clinic"
-      
-      clinic = version.item
-      return false if clinic.blank?
-      
-      if version.event == "create" && clinic.accepting_new_patients?
-        
-        #new clinic
-      
-        if clinic.city.present?
-          return "#{link_to clinic.name, clinic_path(clinic), :class => 'ajax'} (#{clinic.specializations.map{ |s| s.name }.to_sentence}) is accepting patients in #{clinic.city}. #{version.id}".html_safe
-        else
-          return "#{link_to clinic.name, clinic_path(clinic), :class => 'ajax'} (#{clinic.specializations.map{ |s| s.name }.to_sentence}) is accepting patients. #{version.id}".html_safe
-        end
-      
-      end
-      
-    elsif version.item_type == "Attendance"
-      
-      attendance = version.item
-      return false if attendance.blank? || attendance.specialist.blank? || attendance.clinic.blank?
+    Version.order("id desc").where("item_type = ? OR item_type = ? OR item_type = ? OR item_type = ? OR item_type = ? OR item_type = ?", "Specialist", "Clinic", "Attendance", "Schedule", "Address", "SpecialistOffice").limit(200).each do |version|
     
-      specialist = attendance.specialist;
-      clinic = attendance.clinic;
+      next if version.item.blank?
+      break if events.length >= max_events
       
-      if version.event == "create"
+      begin
+      
+        if version.item_type == "Specialist"
+          
+          specialist = version.item
+          next if specialist.blank?
+          
+          if version.event == "create" && specialist.accepting_new_patients?
+            
+            #new specialist that is accepting patients
+            
+            if specialist.city.present? 
+              events["#{version.item_type}_#{version.item.id}"] = "#{link_to specialist.name, specialist_path(specialist), :class => 'ajax'} (#{specialist.specializations.map{ |s| s.name }.to_sentence}) is accepting patients in #{specialist.city}. #{version.id}".html_safe
+            else 
+              events["#{version.item_type}_#{version.item.id}"] = "#{link_to specialist.name, specialist_path(specialist), :class => 'ajax'} (#{specialist.specializations.map{ |s| s.name }.to_sentence}) is accepting patients. #{version.id}".html_safe
+            end
+          
+          elsif version.event == "update"
+          
+            if specialist.retired?
+              
+              next if version.previous.blank? || version.previous.reify.blank?
+              next if version.previous.reify.retired? #retired status hasn't changed
+              
+              #newly retired
+              
+              events["#{version.item_type}_#{version.item.id}"] = "#{version.item_type}_#{version.item.id}", "#{link_to specialist.name, specialist_path(specialist), :class => 'ajax'} (#{specialist.specializations.map{ |s| s.name }.to_sentence}) has retired. #{version.id}".html_safe
+              
+            elsif specialist.retiring?
+              
+              next if version.previous.blank? || version.previous.reify.blank?
+              next if version.previous.reify.retiring? #retiring status hasn't changed
+              
+              events["#{version.item_type}_#{version.item.id}"] = "#{link_to specialist.name, specialist_path(specialist), :class => 'ajax'} (#{specialist.specializations.map{ |s| s.name }.to_sentence}) is retiring on #{specialist.unavailable_from.strftime('%B %d, %Y')}. #{version.id}".html_safe
+              
+            end
+          
+          end
+          
+        elsif version.item_type == "Clinic"
+          
+          clinic = version.item
+          next if clinic.blank?
+          
+          if version.event == "create" && clinic.accepting_new_patients?
+            
+            #new clinic
+          
+            if clinic.city.present?
+              events["#{version.item_type}_#{version.item.id}"] = "#{link_to clinic.name, clinic_path(clinic), :class => 'ajax'} (#{clinic.specializations.map{ |s| s.name }.to_sentence}) is accepting patients in #{clinic.city}. #{version.id}".html_safe
+            else
+              events["#{version.item_type}_#{version.item.id}"] = "#{link_to clinic.name, clinic_path(clinic), :class => 'ajax'} (#{clinic.specializations.map{ |s| s.name }.to_sentence}) is accepting patients. #{version.id}".html_safe
+            end
+          
+          end
+          
+        elsif version.item_type == "Attendance"
+          
+          attendance = version.item
+          next if attendance.blank? || attendance.specialist.blank? || attendance.clinic.blank?
         
-        #new specialist / clinic association
-      
-        return "#{link_to specialist.name, specialist_path(specialist), :class => 'ajax'} (#{specialist.specializations.map{ |s| s.name }.to_sentence}) now works at the #{link_to clinic.name, clinic_path(clinic), :class => 'ajax'}. #{version.id}".html_safe
-      
+          specialist = attendance.specialist;
+          clinic = attendance.clinic;
+          
+          if version.event == "create"
+            
+            #new specialist / clinic association
+          
+            events["#{version.item_type}_#{specialist.id}"] = "#{link_to specialist.name, specialist_path(specialist), :class => 'ajax'} (#{specialist.specializations.map{ |s| s.name }.to_sentence}) now works at the #{link_to clinic.name, clinic_path(clinic), :class => 'ajax'}. #{version.id}".html_safe
+          
+          end
+          
+        elsif version.item_type == "Schedule"
+          
+          schedule = version.item
+          next if schedule.blank? || !schedule.scheduled? || schedule.schedulable.blank?
+        
+          schedulable = schedule.schedulable
+          
+          if version.event == "create" || version.event == "update"
+          
+            events["Clinic_#{schedulable.id}"] = "The schedule for the #{link_to schedulable.name, url_for(schedulable), :class => 'ajax'} (#{schedulable.specializations.map{ |s| s.name }.to_sentence}) is now #{schedule.days_and_hours.to_sentence}. #{version.id}".html_safe
+          
+          end
+          
+        elsif version.item_type == "Address"
+          
+          address = version.item
+          next if address.blank? || address.locations.blank?
+          
+          location = address.locations.first
+          next if location.blank?
+          
+          next if version.previous.blank? || version.previous.reify.blank?
+          old_address = version.previous.reify
+          
+          next if old_address.short_address == address.short_address #hasn't changed
+          
+          if version.event == "update"
+            
+            if location.locatable.is_a?(Office)
+          
+              events["#{version.item_type}_#{version.item.id}"] = "The address for #{location.locatable.specialists.map{ |s| link_to s.name, specialist_path(s), :class => 'ajax' }.to_sentence} is now #{address.short_address}. #{version.id}".html_safe
+              
+            else
+              
+              events["#{version.item_type}_#{version.item.id}"] = "The address for #{location.locatable.name} is now #{address.short_address}. #{version.id}".html_safe
+            
+            end
+          
+          end
+          
+        elsif version.item_type == "SpecialistOffice"
+        
+          specialist_office = version.item
+          next if specialist_office.blank? || specialist_office.specialist.blank? || specialist_office.office.blank?
+          
+          specialist = specialist_office.specialist
+          office = specialist_office.office
+            
+          next if office.short_address.blank?
+          
+          if version.event == "create" || version.event == "update"
+            
+            if office.specialists.length > 1
+              
+              events["Specialist_#{specialist.id}"] = "#{link_to specialist.name, specialist_path(specialist), :class => 'ajax'} (#{specialist.specializations.map{ |s| s.name }.to_sentence }) now works in an office at #{office.short_address}. #{version.id} with #{office.specialists.reject{ |s| specialist.id == s.id }.map{ |s| link_to s.name, specialist_path(s), :class => 'ajax' }.to_sentence}. #{version.id}".html_safe
+            
+            else
+              
+              events["Specialist_#{specialist.id}"] = "#{link_to specialist.name, specialist_path(specialist), :class => 'ajax'} (#{specialist.specializations.map{ |s| s.name }.to_sentence }) now works in an office at #{office.short_address}. #{version.id}".html_safe
+            
+            end
+          end
+        end
+      rescue Exception => exc
+        #events['error'] = exc
+        next
       end
     end
-    rescue Exception => exc
-      return false
+    
+    return events.values
   end
   
 end
