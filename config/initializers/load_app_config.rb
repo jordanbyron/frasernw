2f19b197bcde4887b535c42359bf67503a1c42db
Russell Warneboldt <warneboldt@gmail.com>
2012-06-22 09:58:38 -0700

    Fixing capitalization

--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -2,5 +2,5 @@ raw_config = File.read("#{Rails.root}/config/app_config.yml")
 APP_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
 
 if Rails.env.production?
-  APP_CONFIG[:domain] = ENV['domain']
+  APP_CONFIG[:domain] = ENV['DOMAIN']
 end
