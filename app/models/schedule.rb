6963b4aa1389fc7b20be1d51b45206084c9e1aae
Russell Warneboldt <warneboldt@gmail.com>
2012-07-07 10:22:10 -0700

    Better handling of schedules display. Allows "blank" hours.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -42,13 +42,63 @@ class Schedule < ActiveRecord::Base
   
   def days_and_hours
     output = []
-    output << "Monday #{monday.from.to_s(:schedule_time)} - #{monday.to.to_s(:schedule_time)}" if monday.scheduled
-    output << "Tuesday #{tuesday.from.to_s(:schedule_time)} - #{tuesday.to.to_s(:schedule_time)}" if tuesday.scheduled
-    output << "Wednesday #{wednesday.from.to_s(:schedule_time)} - #{wednesday.to.to_s(:schedule_time)}" if wednesday.scheduled
-    output << "Thursday #{thursday.from.to_s(:schedule_time)} - #{thursday.to.to_s(:schedule_time)}" if thursday.scheduled
-    output << "Friday #{friday.from.to_s(:schedule_time)} - #{friday.to.to_s(:schedule_time)}" if friday.scheduled
-    output << "Saturday #{saturday.from.to_s(:schedule_time)} - #{saturday.to.to_s(:schedule_time)}" if saturday.scheduled
-    output << "Sunday #{sunday.from.to_s(:schedule_time)} - #{sunday.to.to_s(:schedule_time)}" if sunday.scheduled
+    
+    [[monday, "Monday"], [tuesday, "Tuesday"], [wednesday, "Wednesday"], [thursday, "Thursday"], [friday, "Friday"], [saturday, "Saturday"], [sunday, "Sunday"]].each do |var|
+      day = var.first
+      day_name = var.second
+      if day.scheduled? && day.time?
+        output << "#{day_name} #{day.time}"
+      elsif day.scheduled?
+        output << day_name
+      end
+    end
+      
+    return output
+  end
+  
+  DAY_HASH = { 
+    1 => "Monday", 
+    2 => "Tuesday",
+    3 => "Wednesday",
+    4 => "Thursday",
+    5 => "Friday",
+    6 => "Saturday",
+    7 => "Sunday", 
+  }
+  
+  def collapsed_days_and_hours
+    
+    collapsed = {}
+    
+    [[monday, 1], [tuesday, 2], [wednesday, 3], [thursday, 4], [friday, 5], [saturday, 6], [sunday, 7]].each do |var|
+      day = var.first
+      day_id = var.second
+      next if !day.scheduled?
+      index = day.time? ? day.time : -1
+      collapsed[index] = [collapsed[index], day_id].flatten.compact
+    end
+    
+    output = []
+    
+    collapsed.each do |hours, day_ids|
+      #difference between each day and the next. If it's all 1's (except the last element, where we wrapped) then we have consecutive days.
+      consecutive = true
+      day_ids.zip(day_ids.rotate).map { |x, y| y - x }.take( day_ids.size - 1 ).map{ |diff| consecutive &= (diff == 1) }
+      
+      #map our days to a sentence
+      days = day_ids.map{ |id| DAY_HASH[id] }.to_sentence
+      
+      if day_ids.length >= 3 && consecutive
+        days = "#{DAY_HASH[day_ids.first]} - #{DAY_HASH[day_ids.last]}"
+      end
+      
+      if hours != -1
+        output << "#{days}: #{hours}"
+      else
+        output << "#{days}"
+      end
+    end
+    
     return output
   end
 end
--- app/models/schedule_day.rb
+++ app/models/schedule_day.rb
@@ -2,4 +2,12 @@ class ScheduleDay < ActiveRecord::Base
   attr_accessible :scheduled, :from, :to
   has_one :schedule
   has_paper_trail
+  
+  def time?
+    from.hour != 0 || from.min != 0 || to.hour != 0 || to.min != 0
+  end
+  
+  def time
+    "#{from.to_s(:schedule_time)} - #{to.to_s(:schedule_time)}"
+  end
 end
