f2ca74cdfe47c82582f2cf2401488f007e7b2478
Russell Warneboldt <warneboldt@gmail.com>
2012-07-30 17:11:22 -0700

    cosmetic code change

--- db/migrate/20120730235201_add_more_indexes.rb
+++ db/migrate/20120730235201_add_more_indexes.rb
@@ -8,5 +8,6 @@ class AddMoreIndexes < ActiveRecord::Migration
     
     add_index :sc_item_specialization_procedure_specializations, :sc_item_specialization_id, :name => "index_sc_item_sps_on_sc_item_specialization_id"
     add_index :sc_item_specialization_procedure_specializations, :procedure_specialization_id, :name => "index_sc_item_sps_on_procedure_specialization_id"
+    
   end
 end
