414ac4add7968d4598bced8d4c14f5341303b3f2
Russell Warneboldt <warneboldt@gmail.com>
2012-08-07 11:11:12 -0700

    force path on search caching to handle multiple domains

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   skip_authorization_check
   skip_before_filter :login_required
   
-  caches_action :livesearch
+  caches_action :livesearch, :cache_path => "livesearch"
   
   def livesearch
     respond_to do |format|
