88d069e521ef9e0e0965dc6a826236531dca609c
Russell Warneboldt <warneboldt@gmail.com>
2012-08-12 16:07:24 -0700

    Adding file type and source to content management system display

--- app/models/sc_item.rb
+++ app/models/sc_item.rb
@@ -44,6 +44,71 @@ joins([:sc_item_specializations, :sc_item_specialization_procedure_specializatio
     type_mask == 1
   end
 
+  FORMAT_TYPE_INTERNAL = 0
+  FORMAT_TYPE_EXTERNAL = 1
+  FORMAT_TYPE_HTML = 2
+  FORMAT_TYPE_IMAGE = 3
+  FORMAT_TYPE_PDF = 4
+  FORMAT_TYPE_WORD_DOC = 5
+  FORMAT_TYPE_VIDEO = 6
+
+  FORMAT_HASH = {
+    "pdf" => FORMAT_TYPE_PDF,
+    "html" => FORMAT_TYPE_HTML,
+    "htm" => FORMAT_TYPE_HTML,
+    "php" => FORMAT_TYPE_HTML,
+    "asp" => FORMAT_TYPE_HTML,
+    "doc" => FORMAT_TYPE_WORD_DOC,
+    "png" => FORMAT_TYPE_IMAGE,
+    "tiff" => FORMAT_TYPE_IMAGE,
+    "gif" => FORMAT_TYPE_IMAGE,
+    "jpg" => FORMAT_TYPE_IMAGE,
+    "jpeg" => FORMAT_TYPE_IMAGE,
+    "svg" => FORMAT_TYPE_IMAGE,
+    "www.youtube.com" => FORMAT_TYPE_VIDEO
+  }
+  
+  def format_type
+    if link?
+      ftype = FORMAT_HASH[url.slice(url.rindex('.')+1..-1).downcase]
+      ftype = FORMAT_HASH[domain] if ftype.blank?
+      ftype = FORMAT_TYPE_EXTERNAL if ftype.blank?
+      ftype
+    else
+      FORMAT_TYPE_INTERNAL
+    end
+  end
+
+  def domain
+    if link?
+      require 'uri'
+      URI.parse(url).host
+    else
+      return "Pathways"
+    end
+  end
+
+  def format
+    case format_type
+      when FORMAT_TYPE_INTERNAL
+        "web page"
+      when FORMAT_TYPE_EXTERNAL
+        "web page"
+      when FORMAT_TYPE_HTML
+        "web page"
+      when FORMAT_TYPE_IMAGE
+        "image"
+      when FORMAT_TYPE_PDF
+        "pdf"
+      when FORMAT_TYPE_WORD_DOC
+        "word document"
+      when FORMAT_TYPE_VIDEO
+        "video"
+      else
+        ""
+      end
+  end
+      
   def markdown?
     type_mask == 2
   end
