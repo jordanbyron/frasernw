bc9a085ef86139a7c8b6bd9206b5e3761748be6f
Russell Warneboldt <warneboldt@gmail.com>
2012-08-13 15:39:16 -0700

    Fixing and caching front page for demo

--- app/helpers/front_helper.rb
+++ app/helpers/front_helper.rb
@@ -4,7 +4,7 @@ module FrontHelper
     
     events = {}
         
-    Version.order("id desc").where("item_type = ? OR item_type = ? OR item_type = ? OR item_type = ? OR item_type = ? OR item_type = ?", "Specialist", "Clinic", "Attendance", "Schedule", "Address", "SpecialistOffice").limit(500).each do |version|
+    Version.order("id desc").where("item_type = ? OR item_type = ? OR item_type = ? OR item_type = ? OR item_type = ? OR item_type = ?", "Specialist", "Clinic", "Attendance", "Schedule", "Address", "SpecialistOffice").each do |version|
     
       next if version.item.blank?
       break if events.length >= max_events
