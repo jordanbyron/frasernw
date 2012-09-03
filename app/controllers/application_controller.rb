f0eec0c3c4c9c2c6261efb11c85b9d02adac482f
Russell Warneboldt <warneboldt@gmail.com>
2012-09-03 10:38:35 -0700

    rearrange redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include ControllerAuthentication
-  before_filter [:login_required, :redirect_if_old]
+  before_filter [:redirect_if_old, :login_required]
   protect_from_forgery
   check_authorization
 
