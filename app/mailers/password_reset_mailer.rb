7d68dbc63f0d71239c4d0a3c42c9ff11507add7f
Russell Warneboldt <warneboldt@gmail.com>
2012-09-03 12:44:32 -0700

    send password resets from noreply account

--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -2,7 +2,7 @@ class PasswordResetMailer < ActionMailer::Base
   
   def password_reset_instructions(user)
     @user = user
-    mail(:to => user.email, :from => 'passwordreset@pathwaysbc.ca', :reply_to => 'passwordreset@pathwaysbc.ca', :subject => "Pathways: reset your password")
+    mail(:to => user.email, :from => 'noreply@pathwaysbc.ca', :reply_to => 'noreply@pathwaysbc.ca', :subject => "Pathways: reset your password")
   end
   
 end
