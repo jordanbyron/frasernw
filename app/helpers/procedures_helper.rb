8898afa6e7fbae9c66c65d4e95dd66bb606060c5
Russell Warneboldt <warneboldt@gmail.com>
2012-10-21 09:59:05 -0700

    Fixing ancestry_options name clash

--- app/helpers/procedures_helper.rb
+++ app/helpers/procedures_helper.rb
@@ -1,14 +1,14 @@
 module ProceduresHelper
   
-  def ancestry_options_limited(items, skip_tree, &block)
-    return ancestry_options_limited(items, skip_tree){ |i| "#{'-' * i.depth} #{i.procedure.name}" } unless block_given?
+  def procedure_ancestry_options_limited(items, skip_tree, &block)
+    return procedure_ancestry_options_limited(items, skip_tree){ |i| "#{'-' * i.depth} #{i.procedure.name}" } unless block_given?
     
     result = []
     items.map do |item, sub_items|
       next if skip_tree and skip_tree.include? item
       next if not item.procedure
       result << [yield(item), item.id]
-      result += ancestry_options_limited(sub_items, skip_tree, &block)
+      result += procedure_ancestry_options_limited(sub_items, skip_tree, &block)
     end
     result
   end
