0dbaa2d73ea03bb12cdbf076ec8e01ddd53106c0
Russell Warneboldt <warneboldt@gmail.com>
2012-11-09 17:44:50 -0800

    Further work on per-specialty referral areas: supported when making new divisions, and in admin UI

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -13,14 +13,44 @@ class DivisionsController < ApplicationController
   
   def new
     @division = Division.new
+    @local_referral_cities = {}
+    @division.specializations.each do |s|
+      @local_referral_cities[s.id] = []
+    end
     render :layout => 'ajax' if request.headers['X-PJAX']
   end
   
   def create
     @division = Division.new(params[:division])
     if @division.save
+      @division.division_specializations.each do |ds|
+        #remove existing specializations that no longer exist
+        DivisionSpecialization.destroy(ds.id) if (params[:specialization].blank? || !(params[:specialization].include? ds.specialization_id))
+      end
+      if params[:specialization].present?
+        #add new specializations
+        params[:specialization].each do |specialization_id, set|
+          DivisionSpecialization.find_or_create_by_division_id_and_specialization_id( @division.id, specialization_id )
+        end
+      end
+      @division.division_specialization_cities.each do |dsc|
+        #remove existing cities that no longer exist
+        DivisionSpecializationCity.destroy(dcs.id) if (!(params[:local_referral_city].include? dcs.specialization_id) || !(params[:local_referral_city][dcs.specialization_id].include? dcs.city_id))
+      end
+      if params[:local_referral_city].present?
+        #add new cities
+        params[:local_referral_city].each do |specialization_id, cities|
+          division_specialization = DivisionSpecialization.find_by_division_id_and_specialization_id( @division.id, specialization_id )
+          if division_specialization.present?
+            #parent specialization was checked off
+            cities.each do |city_id, set|
+              DivisionSpecializationCity.find_or_create_by_division_specialization_id_and_city_id( division_specialization.id, city_id )
+            end
+          end
+        end
+      end
       redirect_to @division, :notice => "Successfully created division."
-      else
+    else
       render :action => 'new'
     end
   end
@@ -64,7 +94,7 @@ class DivisionsController < ApplicationController
         end
       end
       redirect_to @division, :notice  => "Successfully updated division."
-      else
+    else
       render :action => 'edit'
     end
   end
