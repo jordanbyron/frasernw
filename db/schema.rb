fda5fe7004f4d1bb639d7dc1e7138f230ddd2697
Russell Warneboldt <warneboldt@gmail.com>
2012-11-19 15:06:22 -0800

    rake

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121116022206) do
+ActiveRecord::Schema.define(:version => 20121119191709) do
 
   create_table "addresses", :force => true do |t|
     t.string    "address1"
