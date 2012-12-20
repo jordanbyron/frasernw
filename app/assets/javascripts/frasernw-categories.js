40eb7a13dafb29f05c63d48622be8ed4532c51c5
Russell Warneboldt <warneboldt@gmail.com>
2012-12-19 18:06:49 -0800

    Inline content items cache and populate too. Specialty and Clinic pages should be complete.

--- app/assets/javascripts/frasernw-categories.js
+++ app/assets/javascripts/frasernw-categories.js
@@ -1,21 +1,26 @@
-function init_category(category_id, procedure_filter)
+function init_category(category_id, inline, procedure_filter)
 {
+  console.log('init_category');
   for(var division_id in filtering.current_divisions)
   {
-    add_items_from_division(filtering.content_data[division_id], category_id, procedure_filter);
+    add_items_from_division(filtering.content_data[division_id], category_id, inline, procedure_filter);
   }
   
-  update_ui(category_id);
+  update_ui(category_id, inline);
 }
 
-function update_ui(category_id)
+function update_ui(category_id, inline)
 {
-  update_subcategories(category_id);
+  console.log('update_ui');
+  if (!inline)
+  {
+    update_subcategories(category_id);
+  }
   $('#' + category_id + '_table').trigger('update');
   $('#specialist_table').trigger('sorton', [[[0,0]]]);
 }
 
-function add_items_from_division(category_data, category_id, procedure_filter)
+function add_items_from_division(category_data, category_id, inline, procedure_filter)
 {
   for(var item_id in category_data[category_id])
   {
@@ -29,42 +34,67 @@ function add_items_from_division(category_data, category_id, procedure_filter)
       }
     }
     var title = item.title;
-    var subcategory = filtering.global_categories[item.subcategory];
+    var full_title = inline ? item.full_title : "";
+    var subcategory = inline ? "" : filtering.global_categories[item.subcategory];
+    var url = item.url;
+    var markdown = inline ? item.markdown : "";
+    var body = inline ? item.body : "";
     var can_email = item.can_email;
     var shared_care = item.shared_care;
     var attributes = item.attributes;
-    add_item(category_id, item_id, title, subcategory, can_email, shared_care, attributes);
+    add_item(category_id, item_id, title, full_title, url, markdown, body, subcategory, can_email, shared_care, attributes, inline);
   }
 }
 
-function add_item(category_id, item_id, title, subcategory, can_email, shared_care, attributes)
+function add_item(category_id, item_id, title, full_title, url, markdown, body, subcategory, can_email, shared_care, attributes, inline)
 {
-  var row_id = category_id + "_" + item_id;
-  if ($("#" + row_id).length > 0)
+  var entry_id = category_id + "_" + item_id;
+  if ($("#" + entry_id).length > 0)
   {
-    //row already exists
+    //row or item already exists
     return;
   }
   
-  var shared_care_icon = shared_care ? "<i class=\"icon-blue icon-star\"></i>" : ""
   var favorite = "<a href=\"javascript:void(0)\" onclick=\"favorite('content_items'," + item_id + ",'" + title + "')\" title=\"Favourite / un-favourite\"><i class=\"icon-text icon-heart\" id=\"user_favorite_content_items_" + item_id + "\"></i></a>"
-  var email = can_email ? "<a href=\"/content_items/" + item_id + "/email\" class=\"ajax\" title=\"E-mail to patient\"><i class=\"icon-envelope-alt icon-blue\"></i></a>" : ""
   var feedback = "<a href=\"javascript:void(0)\" onclick=\"show_feedback('" + title + "'," + item_id + ")\" title=\"Provide feedback on content\"><i class=\"icon-bullhorn\"></i></a>"
-  var row_html = $("<tr id='" + row_id + "'><td class=\"title\">" + shared_care_icon + "<a href=\"/content_items/" + item_id + "\" class=\"ajax\">" + title + "</a></td><td class=\"subcategory\">" + subcategory + "</td><td class=\"favorite\">" + favorite + "</td><td class=\"email\">" + email + "</td><td class=\"fb\">" + feedback + "</td></tr>");
   
-  if (typeof $.fn.ajaxify !== 'function')
+  if (inline)
   {
-    $('#' + category_id + '_table tr:last').after(row_html);
+    //add it as an inline entry
+    var shared_care_icon = shared_care ? "<i class=\"icon-red icon-star\"></i>" : ""
+    var entry_html = "<div class=\"scm\"><h1>" + shared_care_icon + " " + full_title + " " + favorite + " " + feedback + "</h1>";
+    if (markdown)
+    {
+      entry_html += body;
+    }
+    else
+    {
+      entry_html += "<a href=\"" + url + "\" target=\"_blank\">" + title + "</a>";
+    }
+    $('#' + category_id + '_items').append(entry_html); 
   }
   else
   {
-    $('#' + category_id + '_table tr:last').after(row_html.ajaxify());
+    //add it to the table
+    var shared_care_icon = shared_care ? "<i class=\"icon-blue icon-star\"></i>" : ""
+    var email = can_email ? "<a href=\"/content_items/" + item_id + "/email\" class=\"ajax\" title=\"E-mail to patient\"><i class=\"icon-envelope-alt icon-blue\"></i></a>" : ""
+    var row_html = $("<tr id='" + entry_id + "'><td class=\"title\">" + shared_care_icon + "<a href=\"" + url + "\" class=\"ajax\">" + title + "</a></td><td class=\"subcategory\">" + subcategory + "</td><td class=\"favorite\">" + favorite + "</td><td class=\"email\">" + email + "</td><td class=\"fb\">" + feedback + "</td></tr>");
+    
+    if (typeof $.fn.ajaxify !== 'function')
+    {
+      $('#' + category_id + '_table tr:last').after(row_html);
+    }
+    else
+    {
+      $('#' + category_id + '_table tr:last').after(row_html.ajaxify());
+    }
+    $('#' + entry_id).data('attributes', attributes);
   }
-  $('#' + row_id).data('attributes', attributes);
 }
 
 function update_subcategories(category_id)
 {
+  console.log('update_subcategories');
   var current_subcategories = [];
   
   for(var division_id in filtering.current_divisions)
@@ -73,9 +103,11 @@ function update_subcategories(category_id)
   }
   
   current_subcategories = current_subcategories.unique();
+  console.log('input.ic[name="ic' + category_id + '"]');
   
   //hide subcategories that no item has
-  $("input.ic[name='ic" + category_id + "']").each( function() {
+  $('input.ic[name="ic' + category_id + '"]').each( function() {
+                                                   console.log(this);
     $this = $(this)
     if (current_subcategories.indexOf($this.attr('id')) == -1)
     {
