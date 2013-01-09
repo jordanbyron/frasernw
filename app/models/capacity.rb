700065d89fb978985eeb4838fc89c97f4483f227
Russell Warneboldt <warneboldt@gmail.com>
2013-01-08 16:31:12 -0800

    Specialist display of custom wait times

--- app/models/capacity.rb
+++ app/models/capacity.rb
@@ -3,4 +3,16 @@ class Capacity < ActiveRecord::Base
   belongs_to :procedure_specialization
   
   has_paper_trail
+  
+  def self.specialist_wait_time
+    joins(:procedure_specialization).where('procedure_specializations.specialist_wait_time = (?)', true)
+  end
+
+  def waittime
+    Specialist::WAITTIME_HASH[waittime_mask].present? ? Specialist::WAITTIME_HASH[waittime_mask] : ""
+  end
+
+  def lagtime
+    Specialist::WAITTIME_HASH[lagtime_mask].present? ? Specialist::WAITTIME_HASH[lagtime_mask] : ""
+  end
 end
--- app/models/focus.rb
+++ app/models/focus.rb
@@ -3,4 +3,16 @@ class Focus < ActiveRecord::Base
   belongs_to :procedure_specialization
   
   has_paper_trail
+  
+  def self.clinic_wait_time
+    joins(:procedure_specialization).where('procedure_specializations.clinic_wait_time = (?)', true)
+  end
+
+  def waittime
+    Clinic::WAITTIME_HASH[waittime_mask].present? ? Clinic::WAITTIME_HASH[waittime_mask] : ""
+  end
+
+  def lagtime
+    Clinic::WAITTIME_HASH[lagtime_mask].present? ? Clinic::WAITTIME_HASH[lagtime_mask] : ""
+  end
 end
