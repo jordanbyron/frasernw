eafac7fa94b6cf7d04e3dcb938faf13f264aa604
Russell Warneboldt <warneboldt@gmail.com>
2013-01-20 22:52:18 -0800

    indexOf for IE

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -92,6 +92,17 @@ if (!Array.prototype.map) {
   };      
 }
 
+if(!Array.indexOf) {
+  Array.prototype.indexOf = function(obj) {
+    for (var i=0; i < this.length; i++) {
+      if (this[i] == obj) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
+
 if(typeof String.prototype.trim !== 'function') {
   String.prototype.trim = function() {
     return this.replace(/^\s+|\s+$/g, '');
