71667057b5929c70bb570ff2568555b96571cb12
Russell Warneboldt <warneboldt@gmail.com>
2013-10-06 09:26:25 -0700

    TEMP - setting me to be the email recipient while I test

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -5,14 +5,14 @@ class EventMailer < ActionMailer::Base
     @feedback_item = feedback_item
     item = feedback_item.item
     feedback_provider = feedback_item.user
-    owner = item.owner_for_divisions(feedback_provider.divisions)
+    owner = Owner.find(10) #item.owner_for_divisions(feedback_provider.divisions)
     mail(:to => owner.email, :from => 'noreply@pathwaysbc.ca', :subject => "Pathways: #{item.name} has had feedback left on it")
   end
   
   def mail_content_item_feedback(feedback_item)
     @feedback_item = feedback_item
     item = feedback_item.item
-    owner = item.owner
+    owner = Owner.find(10) #item.owner
     mail(:to => owner.email, :from => 'noreply@pathwaysbc.ca', :subject => "Pathways: #{item.title} has had feedback left on it")
   end
   
@@ -22,11 +22,11 @@ class EventMailer < ActionMailer::Base
     if (review_item.whodunnit.present?)
       #if we know who edited this item lets try to get the owner from their division first
       editied_by_user = User.find(review_item.whodunnit)
-      divisions = left_by_user.divisions
-      owner = item.owner_for_divisions(divisions)
+      divisions = editied_by_user.divisions
+      owner = User.find(10) #item.owner_for_divisions(divisions)
       mail(:to => owner.email, :from => 'noreply@pathwaysbc.ca', :subject => "Pathways: #{item.name} has had been edited by #{editied_by_user.name} and is in the review queue")
     else
-      owner = item.owner_for_divisions(item.divisions)
+      owner = Owner.find(10) #item.owner_for_divisions(item.divisions)
       mail(:to => owner.email, :from => 'noreply@pathwaysbc.ca', :subject => "Pathways: #{item.name} has had been edited using the secret edit link and is in the review queue")
     end
   end
