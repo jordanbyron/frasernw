bfb65a95a6b8c4deed1bed4cd818adec70f78f4e
Russell Warneboldt <warneboldt@gmail.com>
2013-10-20 10:27:57 -0700

    Clinics can have up to 6 locations

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -9,7 +9,7 @@ class Clinic < ActiveRecord::Base
   has_many :specializations, :through => :clinic_specializations
   
   #clinics have multiple locations
-  MAX_LOCATIONS = 2
+  MAX_LOCATIONS = 6
   has_many :clinic_locations, :dependent => :destroy
   accepts_nested_attributes_for :clinic_locations
   has_many :locations, :through => :clinic_locations
