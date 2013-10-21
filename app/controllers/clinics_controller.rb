7b3348f05c80c3fb2dc8ba4635e01299a41b7596
Russell Warneboldt <warneboldt@gmail.com>
2013-10-20 19:30:30 -0700

    Fixing bug with accepting a clinic review item with not areas of practice

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -304,15 +304,20 @@ class ClinicsController < ApplicationController
     
     ClinicSweeper.instance.before_controller_update(@clinic)
     if @clinic.update_attributes(params[:clinic])
-      @clinic.focuses.each do |original_focus|
-        Focus.destroy(original_focus.id) if params[:focuses_mapped][original_focus.procedure_specialization.id.to_s].blank?
-      end
-      params[:focuses_mapped].each do |updated_focus, value|
-        focus = Focus.find_or_create_by_clinic_id_and_procedure_specialization_id(@clinic.id, updated_focus)
-        focus.investigation = params[:focuses_investigations][updated_focus]
-        focus.waittime_mask = params[:focuses_waittime][updated_focus] if params[:focuses_waittime].present?
-        focus.lagtime_mask = params[:focuses_lagtime][updated_focus] if params[:focuses_lagtime].present?
-        focus.save
+      if params[:focuses_mapped].present?
+        @clinic.focuses.each do |original_focus|
+          Focus.destroy(original_focus.id) if params[:focuses_mapped][original_focus.procedure_specialization.id.to_s].blank?
+        end
+        params[:focuses_mapped].each do |updated_focus, value|
+          focus = Focus.find_or_create_by_clinic_id_and_procedure_specialization_id(@clinic.id, updated_focus)
+          focus.investigation = params[:focuses_investigations][updated_focus]
+          focus.waittime_mask = params[:focuses_waittime][updated_focus] if params[:focuses_waittime].present?
+          focus.lagtime_mask = params[:focuses_lagtime][updated_focus] if params[:focuses_lagtime].present?
+          focus.save
+          
+          #save any other focuses that have the same procedure and are in a specialization our clinic is in
+          focus.procedure_specialization.procedure.procedure_specializations.reject{ |ps2| !clinic_specializations.include?(ps2.specialization) }.map{ |ps2| Focus.find_or_create_by_clinic_id_and_procedure_specialization_id(@clinic.id, ps2.id) }.map{ |f| f.save }
+        end
       end
       @clinic.review_object = ActiveSupport::JSON::encode(params)
       @clinic.save
