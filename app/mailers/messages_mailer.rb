f1d9f15068f1eea6184c6c74273ce8836c664f13
Russell Warneboldt <warneboldt@gmail.com>
2014-05-17 10:08:21 -0700

    Fix message mailer not sending more than one email

--- app/mailers/messages_mailer.rb
+++ app/mailers/messages_mailer.rb
@@ -8,12 +8,10 @@ class MessagesMailer < ActionMailer::Base
     primary_contacts = user.divisions.map{ |d| d.primary_contacts}.flatten.uniq
     
     if primary_contacts.present? && (primary_contacts.length > 0)
-      primary_contacts.each do |primary_contact|
-        begin
-          mail(:to => primary_contact.email, :subject => "Pathways: #{message.subject}", :reply_to => message.email)
-        rescue Exception => e
-          mail(:to => 'administration@pathwaysbc.ca', :subject => "Pathways: #{message.subject}", :reply_to => message.email)
-        end
+      begin
+        mail(:to => primary_contacts.map{ |primary_contact| primary_contact.email }, :subject => "Pathways: #{message.subject}", :reply_to => message.email)
+      rescue Exception => e
+        mail(:to => 'administration@pathwaysbc.ca', :subject => "Pathways: #{message.subject}", :reply_to => message.email)
       end
     else
       mail(:to => 'administration@pathwaysbc.ca', :subject => "Pathways: #{message.subject}", :reply_to => message.email)
