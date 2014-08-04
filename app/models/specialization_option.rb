c13d852ac70aab9c58550dee99b5733886f15ea4
Russell Warneboldt <warneboldt@gmail.com>
2014-08-04 10:12:35 -0700

    Initial schema and model change, migration

--- app/models/specialization_option.rb
+++ app/models/specialization_option.rb
@@ -1,10 +1,11 @@
 class SpecializationOption < ActiveRecord::Base
-  attr_accessible :specialization, :owner, :content_owner, :division, :in_progress, :open_to_clinic_tab, :is_new
+  attr_accessible :specialization, :owner, :content_owner, :division, :in_progress, :is_new, :open_to_clinic_tab_old, :open_to_type, :open_to_sc_category
   
   belongs_to :specialization
   belongs_to :owner, :class_name => "User"
   belongs_to :content_owner, :class_name => "User"
   belongs_to :division
+  belongs_to :open_to_sc_category, :class_name => "User"
   
   def self.for_divisions(divisions)
     division_ids = divisions.map{ |d| d.id }
@@ -20,6 +21,16 @@ class SpecializationOption < ActiveRecord::Base
   def self.is_new
     where("specialization_options.is_new = (?)", true)
   end
+
+  OPEN_TO_SPECIALISTS = 1
+  OPEN_TO_CLINICS = 2
+  OPEN_TO_CONTENT_CATEGORY = 3
+
+  OPEN_TO_HASH = {
+    OPEN_TO_SPECIALISTS => "Specialists",
+    OPEN_TO_CLINICS => "Clinics",
+    OPEN_TO_CONTENT_CATEGORY => "Content Category"
+  }
 
   has_paper_trail
 end
