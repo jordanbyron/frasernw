3d45e64ae5b8eeeda714ce5d0e7d2da162045674
kelseydh <kelseyh@gmail.com>
2014-10-11 19:25:54 -0700

    minor: fix whitespace

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,18 +1,18 @@
 class Subscription < ActiveRecord::Base
-	belongs_to :user
+  belongs_to :user
 
   has_many :subscription_divisions, dependent: :destroy
-	has_many :divisions, through: :subscription_divisions
+  has_many :divisions, through: :subscription_divisions
 
-	has_many :news_items, through: :divisions
-	has_many :sc_categories, through: :divisions
+  has_many :news_items, through: :divisions
+  has_many :sc_categories, through: :divisions
 
 
-	has_many :subscription_specializations, dependent: :destroy
-	has_many :specializations, through: :subscription_specializations
+  has_many :subscription_specializations, dependent: :destroy
+  has_many :specializations, through: :subscription_specializations
 
-	accepts_nested_attributes_for :divisions
-	accepts_nested_attributes_for :specializations
+  accepts_nested_attributes_for :divisions
+  accepts_nested_attributes_for :specializations
 	
 
 end
