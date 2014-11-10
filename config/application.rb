9791e9dced17f8b280da55a8b93322b857b9e1ef
kelseydh <kelseyh@gmail.com>
2014-11-10 01:18:19 -0800

    fix incorrect asset loading with change in application.rb; reference for change: http://stackoverflow.com/questions/6005361/sass-import-error-in-rails-3-app-file-to-import-not-found-or-unreadable-comp

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'csv'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+Bundler.require(*Rails.groups(:assets => %w(development test))) if defined?(Bundler)
 
 module Frasernw
   class Application < Rails::Application
