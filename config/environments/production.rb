ed1b4bb57834d6b8f6ae5b1b007d369af247f8a4
kelseydh <kelseyh@gmail.com>
2014-12-19 14:26:29 -0800

    trying scss instead

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Frasernw::Application.configure do
 
   # Compress both stylesheets and JavaScripts
   config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :sass
+  config.assets.css_compressor = :scss
 
   # Specifies the header that your server uses for sending files
   # (comment out if your front-end server doesn't support this)
@@ -45,10 +45,10 @@ Frasernw::Application.configure do
   ##Added for Rails3.0 to 3.1
   # Compress JavaScripts and CSS
   config.assets.compress = true
-   
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-   
+
   # Generate digests for assets URLs
   config.assets.digest = true
   ##
