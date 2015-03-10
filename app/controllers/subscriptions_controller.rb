b9ad6e90de22009eb8ac37491d9033bba0f62f17
kelseydh <kelseyh@gmail.com>
2015-03-10 01:34:53 -0400

    fix routing for subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -50,7 +50,7 @@ class SubscriptionsController < ApplicationController
 
     respond_to do |format|
       if @subscription.save
-        format.html { redirect_to "index", notice: 'Subscription was successfully created.' }
+        format.html { redirect_to subscriptions_url, notice: 'Subscription was successfully created.' }
         format.json { render json: @subscription, status: :created, location: @subscription }
       else
         format.html { render action: "new" }
@@ -66,10 +66,10 @@ class SubscriptionsController < ApplicationController
 
     respond_to do |format|
       if @subscription.update_attributes(params[:subscription])
-        format.html { redirect_to "index", notice: 'Subscription was successfully updated.' }
+        format.html { redirect_to subscriptions_url, notice: "Subscription was successfully updated." }
         format.json { head :ok }
       else
-        format.html { render action: "edit" }
+        format.html { render action: "edit", notice: "Sorry, there was an error updating your subscription." }
         format.json { render json: @subscription.errors, status: :unprocessable_entity }
       end
     end
