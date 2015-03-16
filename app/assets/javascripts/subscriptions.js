9b2aad15487b487015bd6e6fa26255979342c885
kelseydh <kelseyh@gmail.com>
2015-03-16 04:18:45 -0400

    improve subscription edit form

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -4,42 +4,94 @@
 $(document).ready(function() {
 
 
-  $("#subscription_classification_news_updates").change(function(){ 
+  $("#subscription_classification_news_updates").change(function (){ 
     console.log("news update change");
     update_for_classification_change();
   });
 
-  $("#subscription_classification_resource_updates").change(function(){ 
+  $("#subscription_classification_resource_updates").change(function (){ 
     console.log("resource update change");
     update_for_classification_change();
   });
 
 
-  var update_for_classification_change = function()
+  var update_for_classification_change = function ()
   {
     $('.all-updates').show(); // show form submit options again
     if ($("#subscription_classification_news_updates").is(":checked"))
     {
       console.log("news update checked");
-      $(".news-updates").show();
       $(".resource-updates").hide();
+      $(".news-updates").fadeIn(300).show();
+
     }
     else if ($("#subscription_classification_resource_updates").is(":checked"))
     {
       console.log("resource update checked");
       $(".news-updates").hide();
-      $(".resource-updates").show();
+      $(".resource-updates").fadeIn(300).show();
     }
 
   }
-  // var news_updates_changed = function(){
-  //   alert("news_updates_changed");
+
+
+$('#all_news_types').on('click', function(e) {
+     $('.news_type-option').prop('checked', $(e.target).prop('checked'));
+ });
+
+$('#all_sc_categories').on('click', function(e) {
+     $('.sc_category-option').prop('checked', $(e.target).prop('checked'));
+ });
+
+$('#all_specializations').on('click', function(e) {
+     $('.specialization-option').prop('checked', $(e.target).prop('checked'));
+ });
+
+$('#all_divisions').on('click', function(e) {
+     $('.division-option').prop('checked', $(e.target).prop('checked'));
+ });
+
+
+
+// $("#subscription_specialization_ids_" + value).prop("checked", true);
+
+
+  // $.fn.fix_radios = function () {
+  //   function focus() {
+  //     if ( !this.checked ) return;
+  //     if ( !this.was_checked ) {
+  //       $( this ).change();
+  //     }
+  //   }
+
+  //   function change( e ) {
+  //     if ( this.was_checked ) {
+  //       e.stopImmediatePropagation();
+  //       return;
+  //     }
+  //     $( "input[name=" + this.name + "]" ).each( function() {
+  //       this.was_checked = this.checked;
+  //     } );
+  //   }
+  //   return this.focus( focus ).change( change );
   // }
-  //$("#subscription_classification_news_updates").live("change", resource_updates_changed);
 
-  //$("#subscription_classification_resource_updates").live("change", news_updates_changed);
 
-  //$("#subscription_classification_resource_updates").prop('checked', true).show()
+  // $(function() {
+  //   $( "input[type=radio]" ).fix_radios();
+  //   $("input[name='subscription[classification]']").change(function(){
+  //     if ($("input[name='subscription[classification]']:checked").val() == 'News Updates'){
+  //       $(".news-updates").show();
+  //       $(".resource-updates").hide();
+  //     } else {
+  //       alert("do other stuff");
+  //       $(".news-updates").hide();
+  //       $(".resource-updates").show();
+  //     }
+  //   });
+  // });
+
+
 
 });
 
