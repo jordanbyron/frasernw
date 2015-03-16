0de4fe1e1e1e570d069e908c7e0f7ad5adbb609f
kelseydh <kelseyh@gmail.com>
2015-03-16 07:42:29 -0400

    update subscriptions controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,12 +1,9 @@
 class SubscriptionsController < ApplicationController
  load_and_authorize_resource
-
   # GET /subscriptions
   # GET /subscriptions.json
   def index
-    @subscriptions = Subscription.all
-    @owned_news_items = NewsItem.in_divisions(current_user_divisions).paginate(:page => params[:owned_page], :per_page => 30)
-    @other_news_items = NewsItem.in_divisions(Division.all - current_user_divisions).paginate(:page => params[:other_page], :per_page => 30)
+    @subscriptions = current_user.subscriptions.includes(:specializations, :divisions, :sc_categories).all
     render :layout => 'ajax' if request.headers['X-PJAX']
 
     # respond_to do |format|
@@ -18,7 +15,7 @@ class SubscriptionsController < ApplicationController
   # GET /subscriptions/1
   # GET /subscriptions/1.json
   def show
-    @subscription = Subscription.find(params[:id])
+    @subscription = current_user.subscriptions.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -29,9 +26,7 @@ class SubscriptionsController < ApplicationController
   # GET /subscriptions/new
   # GET /subscriptions/new.json
   def new
-    @user = current_user
-    @subscription = Subscription.new(user_id: @user.id)
-    #@subscription.subscription_news_item_type.build unless @subscription.subscription_news_item_type.present?
+    @subscription = current_user.subscriptions.new
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @subscription }
@@ -40,14 +35,14 @@ class SubscriptionsController < ApplicationController
 
   # GET /subscriptions/1/edit
   def edit
-    @subscription = Subscription.find(params[:id])
+    @subscription = current_user.subscriptions.find(params[:id])
   end
 
   # POST /subscriptions
   # POST /subscriptions.json
   def create
     @subscription = Subscription.new(params[:subscription])
-
+    @subscription.user = current_user
     respond_to do |format|
       if @subscription.save
         format.html { redirect_to subscriptions_url, notice: 'Subscription was successfully created.' }
@@ -62,8 +57,7 @@ class SubscriptionsController < ApplicationController
   # PUT /subscriptions/1
   # PUT /subscriptions/1.json
   def update
-    @subscription = Subscription.find(params[:id])
-
+    @subscription = current_user.subscriptions.find(params[:id])
     respond_to do |format|
       if @subscription.update_attributes(params[:subscription])
         format.html { redirect_to subscriptions_url, notice: "Subscription was successfully updated." }
@@ -79,11 +73,22 @@ class SubscriptionsController < ApplicationController
   # DELETE /subscriptions/1.json
   def destroy
     @subscription = Subscription.find(params[:id])
-    @subscription.destroy
-
-    respond_to do |format|
-      format.html { redirect_to subscriptions_url }
-      format.json { head :ok }
+    if @subscription.user == current_user && @subscription.destroy
+      respond_to do |format|
+        format.html { redirect_to subscriptions_url }
+        format.json { head :ok }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to @subscription, alert: "We had trouble deleting this subscription"}
+        format.json { render json: @subscription.errors, status: :unprocessable_entity }
+      end
     end
+  end
+
+  private
+  # add custom param parsing here
+  def subscription_params
+    params[:subscription]
   end
 end
