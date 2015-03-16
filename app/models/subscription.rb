d63bf9de3c9949054f4a14a9800b825c092b8ab8
kelseydh <kelseyh@gmail.com>
2015-03-16 07:45:18 -0400

    subscriptions index now has summary table of options selected; refactor of existing subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -21,7 +21,7 @@ class Subscription < ActiveRecord::Base
   scope :daily,   -> {where(interval: INTERVAL_DAILY)}
   scope :weekly,  -> {where(interval: INTERVAL_WEEKLY)}
   scope :monthly, -> {where(interval: INTERVAL_MONTHLY)}
-  scope :immediate, -> {where(interval: INTERVAL_IMMEDIATE)}
+  scope :immediate, -> {where(interval: INTERVAL_IMMEDIATELY)}
 
   accepts_nested_attributes_for :divisions
   accepts_nested_attributes_for :sc_categories
@@ -40,16 +40,16 @@ class Subscription < ActiveRecord::Base
     RESOURCE_UPDATES => "Resource Updates".freeze
   }
 
-  INTERVAL_IMMEDIATE = 1
+  INTERVAL_IMMEDIATELY = 1
   INTERVAL_DAILY     = 2
   INTERVAL_WEEKLY    = 3
   INTERVAL_MONTHLY   = 4
 
   INTERVAL_HASH = {
+    INTERVAL_IMMEDIATELY => "Immediately".freeze,
     INTERVAL_DAILY => "Daily".freeze,
     INTERVAL_WEEKLY => "Weekly".freeze,
-    INTERVAL_MONTHLY => "Monthly".freeze,
-    INTERVAL_IMMEDIATE => "Immediate".freeze
+    INTERVAL_MONTHLY => "Monthly".freeze
   }
 
   def interval_type
@@ -61,11 +61,19 @@ class Subscription < ActiveRecord::Base
   end
 
   def news_type_masks
-    news_type.reject(&:empty?).map{|nt| nt.to_i}
+    news_type.reject(&:empty?).map(&:to_i)
   end
 
-  def news_type_masks_strings
+  def news_types_pluralized
+    news_type_strings.map(&:pluralize)
+  end
+
+  def news_type_strings
     news_type_masks.map{|nt| Subscription::NEWS_ITEM_TYPE_HASH[nt]}
+  end
+
+  def news_type_present?
+    news_type.reject(&:blank?).present?
   end
 
   def self.news_update
@@ -78,7 +86,7 @@ class Subscription < ActiveRecord::Base
 
   def interval_to_datetime
     case interval
-    when ::Subscription::INTERVAL_IMMEDIATE
+    when ::Subscription::INTERVAL_IMMEDIATELY
       return 1.hours.ago
     when ::Subscription::INTERVAL_DAILY
       return 1.days.ago
@@ -91,8 +99,8 @@ class Subscription < ActiveRecord::Base
 
   def interval_to_words
     case interval
-    when ::Subscription::INTERVAL_IMMEDIATE
-      return "Immediate".freeze
+    when ::Subscription::INTERVAL_IMMEDIATELY
+      return "Immediately"
     when ::Subscription::INTERVAL_DAILY
       return "Daily"
     when ::Subscription::INTERVAL_WEEKLY
@@ -100,5 +108,14 @@ class Subscription < ActiveRecord::Base
     when ::Subscription::INTERVAL_MONTHLY
       return "Monthly"
     end
+  end
+
+  #decorate data
+  def specializations_comma_separated
+    specializations.map(&:name).join(", ")
+  end
+
+  def news_item_type_hash_array
+    news_type.reject(&:blank?).map(&:to_i).map{|n| Subscription::NEWS_ITEM_TYPE_HASH[n]}
   end
 end
