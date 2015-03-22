6c1b7ba76405bda7d3dc0871cb571d3c25ed1d4d
kelseydh <kelseyh@gmail.com>
2015-03-22 01:55:59 -0400

    merge update_classifications in mailer task

--- app/models/subscription_user_worker.rb
+++ app/models/subscription_user_worker.rb
@@ -3,8 +3,10 @@ class SubscriptionUserWorker < ActiveRecord::Base
   def self.mail_subscriptions_by_date!(date_interval) #date_interval is integer value in Subscription
     User.with_subscriptions.each do |user|
       next unless user.subscriptions.present?
-      user.subscriptions_by_date_interval(date_interval).each do |subscription|
-        SubscriptionWorker.mail_subscriptions!(subscription)
+      Subscription::UPDATE_CLASSIFICATION_HASH.map do |key, value| # send merged emails by classification & date
+        subscriptions = (user.subscriptions_by_classification(value) & user.subscriptions_by_date_interval(date_interval))
+        next unless subscriptions.present?
+        SubscriptionWorker.mail_subscriptions!(subscriptions)
       end
     end
   end
