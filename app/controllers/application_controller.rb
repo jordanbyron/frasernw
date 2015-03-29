91ed5ec395e6ff61d39aa3364ed9665fb0463e3e
Russell Warneboldt <warneboldt@gmail.com>
2015-03-29 09:53:20 -0700

    Fix another bad file merge.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 class ApplicationController < ActionController::Base
   include ControllerAuthentication
-  before_filter :redirect_to_cedar
+  before_filter :login_required
   protect_from_forgery
   check_authorization
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => "You are not allowed to access this page"
-  end
-
-  def redirect_to_cedar
-    redirect_to "http://pathwaysbc.herokuapp.com", :status => :moved_permanently
   end
 end
