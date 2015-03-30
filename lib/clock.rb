194044634d6766d98a4581a872a550e060c17c26
kelseydh <kelseyh@gmail.com>
2015-03-30 03:10:26 -0400

    update cron job, insert test mail task

--- lib/clock.rb
+++ lib/clock.rb
@@ -4,32 +4,29 @@ require 'clockwork'
 
 include Clockwork
 
-  # You can trigger rake tasks like this:
-  every(1.minutes, 'bundle exec rake jobs:workoff') {
-    `bundle exec rake jobs:workoff`
-  }
+  # # You can trigger rake tasks like this:
+  # every(1.minutes, 'bundle exec rake jobs:workoff') {
+  #   `bundle exec rake pathways:recache:all`
+  # }
 
-  # Temporary Test
-  Clockwork.every(1.day, 'Mail out TEST Monthly Subscriptions job', :at => 'Monday 07:00', :tz => 'UTC', :if => lambda { |t| t.day == 30 }) do
+  # Used for testing:
+  Clockwork.every(1.day, 'Mail out TEST Monthly Subscriptions job', :at => 'Monday 07:20', :tz => 'UTC', :if => lambda { |t| t.day == 30 }) do
    SubscriptionUserWorker.delay.mail_subscriptions_by_date!(Subscription::INTERVAL_MONTHLY)
   end
 
-  # Daily
-  # Mails subscriptions 1st day of month at 2:30pm UTC (14:30), 6:30 am PST
+  # Mails subscriptions DAILY at 2:30pm UTC (14:30), 6:30 am PST
   every(1.day, 'Mail Daily Subscriptions job', :at => '14:30',  :tz => 'UTC') do
     SubscriptionUserWorker.delay.mail_subscriptions_by_date!(Subscription::INTERVAL_DAILY)
   end
 
 
-  # WEEKLY
-  # Mails subscriptions 1st day of month at 2:00pm UTC (14:00), 6:00 am PST
+  # Mail subscriptions WEEKLY on Mondays at 2:00pm UTC (14:00), 6:00 am PST
   every(1.weeks, 'Mail Weekly Subscriptions job', :at => 'Monday 14:00',  :tz => 'UTC') do
     SubscriptionUserWorker.delay.mail_subscriptions_by_date!(Subscription::INTERVAL_WEEKLY)
   end
 
 
-  # MONTHLY
-  # Mail Monthly subscriptions on every 1st day of month at 4:00pm UTC (16:00), 8:00 am PST
+  # Mail subscriptions MONTHLY on 1st day of month at 4:00pm UTC (16:00), 8:00 am PST
   Clockwork.every(1.day, 'Mail out Monthly Subscriptions job', :at => '16:00', :tz => 'UTC', :if => lambda { |t| t.day == 1 }) do
    SubscriptionUserWorker.delay.mail_subscriptions_by_date!(Subscription::INTERVAL_MONTHLY)
   end
