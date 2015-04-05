6922f6b51efa153415b80ea475306ac2f092c79b
kelseydh <kelseyh@gmail.com>
2015-04-05 15:59:39 -0700

    permit model access to format_type public_activity parameter

--- config/initializers/public_activity.rb
+++ config/initializers/public_activity.rb
@@ -1,5 +1,5 @@
 PublicActivity::Activity.class_eval do
-  attr_accessible :update_classification_type, :type_mask, :type_mask_description, :parent_id, :parent_type, :recipient_id, :recipient_type
+  attr_accessible :update_classification_type, :type_mask, :type_mask_description, :parent_id, :parent_type, :recipient_id, :recipient_type, :format_type, :format_type_description
   belongs_to :parent, :polymorphic => true
   after_create do
     SubscriptionUserWorker.mail_subscriptions_by_activity!(self.id)
