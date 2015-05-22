bf340b0b0820a3361e8890cf2e3071de971e78d8
kelseydh <kelseyh@gmail.com>
2015-05-22 16:03:51 -0700

    fix #133, change user checkbox to fix issue of automatic division assignment being missing for admins with only one division

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,10 +5,10 @@ module UsersHelper
       @user.divisions.map(&:id)
     elsif current_user_is_super_admin?
       nil
-    elsif current_user_has_multiple_user_divisions? # default to division of admin user if admin only has 1 division
-      current_user_divisions.first.id
+    elsif current_user_has_multiple_user_divisions? # otherwise keep blank if assignment of division is missing and admin has multiple divisions
+      nil
     else
-      nil #otherwise keep blank if assignment of division is missing and admin has multiple divisions
+      current_user_divisions.first.id  # default to division of admin user if admin only has 1 division
     end
   end
 end
