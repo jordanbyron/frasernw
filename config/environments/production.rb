367f091870f70a0d2b1046214c372e99988fffdc
kelseydh <kelseyh@gmail.com>
2015-05-31 23:06:06 -0700

    still allow rake tasks despite config.threadsafe! mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,8 @@ Frasernw::Application.configure do
   # Enable threaded mode
   config.threadsafe!
 
+  config.dependency_loading = true if $rails_rake_task # added to not break rake tasks after config.threadsafe! http://stackoverflow.com/a/4880253
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
