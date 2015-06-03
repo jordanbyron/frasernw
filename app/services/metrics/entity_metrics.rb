8d9f7a95c894c0ef01581090eab6edc69c9a118f
kelseydh <kelseyh@gmail.com>
2015-06-02 22:23:00 -0700

    add entity metrics CSV table to analytics

--- app/services/metrics/entity_metrics.rb
+++ app/services/metrics/entity_metrics.rb
@@ -1,45 +1,144 @@
 module Metrics
   class EntityMetrics
+    attr_accessor :table
+    def initialize(divisions = [])
+      @divisions = Array.wrap(divisions)
 
-    # attr_accessor :table
-    # def initialize(divisions)
-    #   options = args.extract_options!
-    #   # options[] defines which table columns to use.
+      if @divisions.any?
+        @division_label = @divisions.map{|d| d.name.gsub(/\s+/, "")}.join("_").gsub(/\W/, '-')
+        @specialists = Specialist.in_divisions(@divisions)
+        @clinics = Clinic.in_divisions(@divisions)
+        @procedures = [@specialists + @clinics].flatten.inject([]){ |memo, s| memo.concat( s.procedures ) }.uniq
+        @specializations = Specialization.all.reject{ |s| [s.specialists.in_divisions(@divisions) + s.clinics.in_divisions(@divisions)].flatten.length == 0 }
+        @hospitals = Hospital.in_divisions(@divisions)
+      else
+        @division_label = "All Divisions"
+        @specialists = Specialist.all
+        @clinics = Clinic.all
+        @procedures = Procedure.all
+        @specializations = Specialization.all
+        @hospitals = Hospital.all
+      end
+    end
 
-    #   divisions = Division.find(Array.wrap(divisions))
+    def csv_stamp
+  [[@division_label],
+  ["Pathways Entity Statistics"],
+  ["Generated on #{DateTime.now}"]]
+    end
 
-    #   row[:division]
-    #   row[:]
+    def data
 
-    #   table = Array.new
 
-    #   Clinic.includes([:specializations, :versions, :archived_feedback_items, :feedback_items]).all.each do |clinic|
+      [
+        [
+          "Total"
+        ],
+        [
+          "Specialties",
+          @specializations.length
+        ],
+        [
+          ""
+        ],
+        [
+          "Total Focused & Non Focused",
+          "Focused",
+          "Non Focused",
+          "Assumed for Specialists",
+          "Assumed for Clinics"
+        ],
+        [
+          "Areas of practice",
+          @procedures.length,
+          @procedures.reject{|p| !p.procedure_specializations.first.focused?}.length,
+          @procedures.reject{|p| !p.procedure_specializations.first.nonfocused?}.length,
+          Procedure.all.reject{|p| !p.procedure_specializations.first.assumed_specialist?}.length,
+          Procedure.all.reject{|p| !p.procedure_specializations.first.assumed_clinic?}.length
+        ],
+        [
+          ""
+        ],
+        [
+          "Total",
+          "Completed Survey",
+          "Not Completed Survey",
+          "Purposely Not Yet Surveyed",
+          "Hospital/Clinic Only",
+          "Moved Away",
+          "Retired"
+        ],
+        [
+          "Specialists",
+          @specialists.length,
+          @specialists.reject{|s| !s.responded?}.length,
+          @specialists.reject{|s| !s.not_responded?}.length,
+          @specialists.reject{|s| !s.purposely_not_yet_surveyed?}.length,
+          @specialists.reject{|s| !s.hospital_or_clinic_only?}.length,
+          @specialists.reject{|s| !s.moved_away?}.length,
+          @specialists.reject{|s| !s.retired?}.length
+        ],
+        [
+          ""
+        ],
+        [
+          "Total",
+          "Completed Survey",
+          "Not Completed Survey",
+          "Purposely Not Yet Surveyed"
+        ],
+        [
+          "Clinics",
+          @clinics.length ,
+          @clinics.reject{|s| !s.responded?}.length,
+          @clinics.reject{|s| !s.not_responded?}.length,
+          @clinics.reject{|s| !s.purposely_not_yet_surveyed?}.length
+        ],
+        [
+          ""
+        ],
+        [
+          "Total"
+        ],
+        [
+          "Hospitals",
+          @hospitals.length
+        ],
+        [
+          ""
+        ],
+        [
+          "Specialists by Specialty"
+        ],
+        [
+          "",
+          "Total",
+          "Completed Survey",
+          "Not Completed Survey",
+          "Purposely Not Yet Surveyed",
+          "Hospital/Clinic Only",
+          "Moved Away",
+          "Retired"
+        ]
+      ] + @specializations.inject([]) do |memo, specialization|
+        @specialization_specialists = specialization.specialists & @specialists
+        memo  << [
+          specialization.name,
+          @specialization_specialists.length,
+          @specialization_specialists.reject{|s| !s.responded?}.length,
+          @specialization_specialists.reject{|s| !s.not_responded?}.length,
+          @specialization_specialists.reject{|s| !s.purposely_not_yet_surveyed?}.length,
+          @specialization_specialists.reject{|s| !s.hospital_or_clinic_only?}.length,
+          @specialization_specialists.reject{|s| !s.moved_away?}.length,
+          @specialization_specialists.reject{|s| !s.retired?}.length
+        ]
+      end
 
-    #     clinic_row = Hash.new
-    #     clinic_row[:id]                  = clinic.id
-    #     clinic_row[:name]                = clinic.name
-    #     clinic_row[:specialty]           = clinic.specializations.map{|s| s.name}.join(",")
-    #     clinic_row[:division]            = clinic.divisions.map { |d| d.name  }.join(",")
-    #     clinic_row[:first_version]       = clinic.versions.last.created_at
-    #     clinic_row[:last_version]        = clinic.versions.first.created_at
-    #     clinic_row[:version_number]      = clinic.versions.count
+    end
 
-    #     clinic_row[:feedback_items]      =  ( clinic.feedback_items +
-    #                                           clinic.archived_feedback_items).
-    #                                           map { |f| f.feedback  }.join("  <<<< >>>>  ")
-    #     clinic_row[:feedback_item_count] = (0 + clinic.archived_feedback_items.count + clinic.feedback_items.count)
+    def to_csv_file
+      CSVReport::Service.new("reports/dialogue/entitymetrics/em-#{@division_label}_entity_metrics.csv", (self.csv_stamp + self.data)).exec
+    end
 
-    #     table << clinic_row
-    #   end
-    #   @table = table
-    # end
-
-    # def as_csv
-    #   CSVReport::ConvertHashArray.new(self.table).exec
-    # end
-
-    # def to_csv_file
-    #   CSVReport::Service.new("reports/clinic_metrics.csv", self.as_csv).exec
-    # end
   end
 end
