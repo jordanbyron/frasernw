3dbadcca71e03bce195e68a64da2ca59844db947
kelseydh <kelseyh@gmail.com>
2015-06-12 16:17:44 -0700

    add switch for enabling/disabling bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,3 +1,5 @@
-Bugsnag.configure do |config|
-  config.api_key = ENV['BUGSNAG_API_KEY']
+if ENV['BUGSNAG_ENABLED'].to_b
+  Bugsnag.configure do |config|
+    config.api_key = ENV['BUGSNAG_API_KEY']
+  end
 end
