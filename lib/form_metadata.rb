a3dee090009fe68a1dd11a75ace404000ac25240
Brian Gracie <bgracie@gmail.com>
2015-06-17 11:28:07 -0700

    Prep

--- lib/form_metadata.rb
+++ lib/form_metadata.rb
@@ -1,11 +1,15 @@
 # To help conditionalize by the type of editing for both clinic
 # and specialist forms
+#
 
-# class FormMetadata
-#
-#   secret_edit?
-#   admin_review?
-#   admin_rereview
-#   owner_edit?
-#
-# end
+class FormMetadata
+  # Defined in controller
+  def initialize(interaction_type)
+  end
+
+  secret_edit?
+  admin_review?
+  admin_rereview
+  owner_edit?
+
+end
