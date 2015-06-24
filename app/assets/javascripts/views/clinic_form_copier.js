b8d8b93c925f8a0b673e6806b721468853a6efe3
Brian Gracie <bgracie@gmail.com>
2015-06-24 15:55:08 -0700

    Fix the hidden checkbox inputs

--- app/assets/javascripts/views/clinic_form_copier.js
+++ app/assets/javascripts/views/clinic_form_copier.js
@@ -1,16 +1,10 @@
 (function( $ ) {
   $(document).ready(function() {
     var input = $("input#pre_edit_form_data");
-
-    // Hack with the disabled inputs because they're not picked up by
-    // the jquery plugin
-    var disabledInputs = $("input[disabled='disabled']");
-    disabledInputs.attr("disabled", false);
     var form = $("form.clinic_form");
     var JSONifiedForm = form.serializeJSON();
-    disabledInputs.attr("disabled", true);
-
     var serializedForm = JSON.stringify(JSONifiedForm);
+
     input.val(serializedForm);
   });
 }( window.$ ))
