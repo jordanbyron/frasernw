db9d4cd23033e5dff994dc6a679cc5ceb0c82a32
Brian Gracie <bgracie@gmail.com>
2015-07-03 16:14:24 -0700

    Change reordering cursor

--- app/assets/javascripts/views/faqs.js
+++ app/assets/javascripts/views/faqs.js
@@ -2,6 +2,7 @@
   $(document).ready(function() {
     $(".faqs").sortable({
       axis: "y",
+      cursor: "ns-resize",
       handle: ".faqs__admin_icon--handle",
       update: function() {
         $.post($(this).data("update-url"), $(this).sortable('serialize'))
