f7062d66f60492510346d9795a7ad7e3be3c24a7
Brian Gracie <bgracie@gmail.com>
2015-07-20 13:34:51 -0700

    Allow removal of all areas of practice for specialists

--- app/controllers/specialists_controller.rb
+++ app/controllers/specialists_controller.rb
@@ -66,11 +66,12 @@ class SpecialistsController < ApplicationController
     if @specialist.save!
       if params[:capacities_mapped].present?
         specialist_specializations = @specialist.specializations
-        params[:capacities_mapped].each do |updated_capacity, value|
-          capacity = Capacity.find_or_create_by_specialist_id_and_procedure_specialization_id(@specialist.id, updated_capacity)
-          capacity.investigation = params[:capacities_investigations][updated_capacity]
-          capacity.waittime_mask = params[:capacities_waittime][updated_capacity] if params[:capacities_waittime].present?
-          capacity.lagtime_mask = params[:capacities_lagtime][updated_capacity] if params[:capacities_lagtime].present?
+        params[:capacities_mapped].each do |checkbox_key, value|
+          next unless value == "1"
+          capacity = Capacity.find_or_create_by_specialist_id_and_procedure_specialization_id(@specialist.id, checkbox_key)
+          capacity.investigation = params[:capacities_investigations][checkbox_key]
+          capacity.waittime_mask = params[:capacities_waittime][checkbox_key] if params[:capacities_waittime].present?
+          capacity.lagtime_mask = params[:capacities_lagtime][checkbox_key] if params[:capacities_lagtime].present?
           capacity.save
 
           #save any other capacities that have the same procedure and are in a specialization our specialist is in
--- app/services/update_specialist_capacities.rb
+++ app/services/update_specialist_capacities.rb
@@ -1,15 +1,19 @@
 module UpdateSpecialistCapacities
   def self.exec(specialist, params)
-    if params[:capacities_mapped].present?
-      specialist_specializations = specialist.specializations
-      specialist.capacities.each do |original_capacity|
-        Capacity.destroy(original_capacity.id) if params[:capacities_mapped][original_capacity.procedure_specialization.id.to_s].blank?
+    return unless params[:capacities_mapped].present?
+
+    specialist_specializations = specialist.specializations
+    specialist.capacities.each do |original_capacity|
+      if params[:capacities_mapped][original_capacity.procedure_specialization.id.to_s] == "0"
+        original_capacity.destroy
       end
-      params[:capacities_mapped].each do |updated_capacity, value|
-        capacity = Capacity.find_or_create_by_specialist_id_and_procedure_specialization_id(specialist.id, updated_capacity)
-        capacity.investigation = params[:capacities_investigations][updated_capacity]
-        capacity.waittime_mask = params[:capacities_waittime][updated_capacity] if params[:capacities_waittime].present?
-        capacity.lagtime_mask = params[:capacities_lagtime][updated_capacity] if params[:capacities_lagtime].present?
+    end
+    params[:capacities_mapped].each do |checkbox_key, value|
+      if value == "1"
+        capacity = Capacity.find_or_create_by_specialist_id_and_procedure_specialization_id(specialist.id, checkbox_key)
+        capacity.investigation = params[:capacities_investigations][checkbox_key]
+        capacity.waittime_mask = params[:capacities_waittime][checkbox_key] if params[:capacities_waittime].present?
+        capacity.lagtime_mask = params[:capacities_lagtime][checkbox_key] if params[:capacities_lagtime].present?
         capacity.save
 
         #save any other capacities that have the same procedure and are in a specialization our specialist is in
