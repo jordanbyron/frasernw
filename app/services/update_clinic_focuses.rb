d97b16ccc7d278b73f486595c6369464b7714365
Brian Gracie <bgracie@gmail.com>
2015-07-22 14:53:33 -0700

    Account for clinics with multiple specializations when destroying focuses

--- app/services/update_clinic_focuses.rb
+++ app/services/update_clinic_focuses.rb
@@ -1,10 +1,20 @@
-module UpdateClinicFocuses
+class UpdateClinicFocuses
+  attr_reader :clinic, :params
+
   def self.exec(clinic, params)
+    new(clinic, params).exec
+  end
+
+  def initialize(clinic, params)
+    @clinic = clinic
+    @params = params
+  end
+
+  def exec
     return unless params[:focuses_mapped].present?
 
     clinic.focuses.each do |focus|
-      focus_checkbox_key = focus.procedure_specialization.id.to_s
-      if params[:focuses_mapped][focus_checkbox_key] == "0"
+      if params_indicate_destroy_focus?(focus)
         focus.destroy
       end
     end
@@ -25,5 +35,19 @@ module UpdateClinicFocuses
       #save any other focuses that have the same procedure and are in a specialization our clinic is in
       focus.procedure_specialization.procedure.procedure_specializations.reject{ |ps2| !clinic_specializations.include?(ps2.specialization) }.map{ |ps2| Focus.find_or_create_by_clinic_id_and_procedure_specialization_id(clinic.id, ps2.id) }.map{ |f| f.save }
     end
+  end
+
+  def params_indicate_destroy_focus?(focus)
+    focus.
+      procedure.
+      procedure_specializations.
+      map(&:id).
+      map(&:to_s).each do |id|
+        if params[:focuses_mapped][id] == "0"
+          return true
+        end
+      end
+
+    false
   end
 end
