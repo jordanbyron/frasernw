006ca9b7393b26e507703c72266dd6ee9ff6012a
Brian Gracie <bgracie@gmail.com>
2015-08-12 14:57:36 -0700

    Introduce cell generators

--- app/assets/javascripts/modules/row_generators.js
+++ app/assets/javascripts/modules/row_generators.js
@@ -1,15 +1,17 @@
 (function (pathways) {
-  var attr = function(attrName) {
-    return function(record) {
-      return record[attrName]
+  var cellGenerators = {
+    attr: function(attrName) {
+      return function(record) {
+        return record[attrName]
+      }
     }
   }
 
   window.pathways.rowGenerators = {
     exampleTable: [
-      attr("id"),
-      attr("name"),
-      attr("date"),
+      cellGenerators.attr("id"),
+      cellGenerators.attr("name"),
+      cellGenerators.attr("date"),
       function(record) { return "www.google.ca" }
     ]
   }
