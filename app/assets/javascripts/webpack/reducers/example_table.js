f58d241420829293ce40c358efcde559dcab85ba
Brian Gracie <bgracie@gmail.com>
2015-08-18 15:41:00 -0700

    Compose reducers

--- app/assets/javascripts/webpack/reducers/example_table.js
+++ app/assets/javascripts/webpack/reducers/example_table.js
@@ -1,13 +1,5 @@
 var objectAssign = require("object-assign");
 
-var initialState = {
-  tableHeadings: [],
-  records: [],
-  filters: {city: {}},
-  filterVisibility: {city: true},
-  cityIndex: {}
-};
-
 var setAllOwnValues = function(obj, value) {
   for (var key in obj) {
     if (obj.hasOwnProperty(key)) {
@@ -21,55 +13,94 @@ var setAllOwnValues = function(obj, value) {
 var changeCitySelection = function(state, newValues) {
   var cityFilters = objectAssign(
     {},
-    state.filters.city
+    state.city
   )
   cityFilters = setAllOwnValues(cityFilters, newValues);
 
-  var newFilters = objectAssign(
+  return objectAssign(
     {},
-    state.filters,
+    state,
     { city: cityFilters}
-  )
-
-  return objectAssign({}, state, { filters: newFilters} );
+  );
 }
 
-module.exports = function(state = initialState, action) {
-  console.log(action);
-  switch (action.type) {
+var filters = function(state={city:{}}, action) {
+  switch(action.type) {
   case "INITIALIZE_FROM_SERVER":
-    return objectAssign({}, action.initialState);
-  case "TOGGLE_FILTER_VISIBILITY":
-    var newSetting = {};
-    newSetting[action.filterKey] = !state.filterVisibility[action.filterKey];
-
-    var newFilterVisibility = objectAssign(
+    return action.initialState.filters
+  case "FILTER_UPDATED":
+    var newFilter = objectAssign(
       {},
-      state.filterVisibility,
-      newSetting
-    )
+      state[action.filterType],
+      { [action.filterKey]: action.filterValue }
+    );
 
-    return objectAssign({}, state, {filterVisibility: newFilterVisibility});
+    return objectAssign(
+      {},
+      state,
+      { [action.filterType]: newFilter }
+    );
   case "SELECT_ALL_CITIES":
     return changeCitySelection(state, true);
   case "DESELECT_ALL_CITIES":
     return changeCitySelection(state, false);
-  case "FILTER_UPDATED":
-    var newFilter = objectAssign(
-      {},
-      state.filters[action.filterType],
-      { [action.filterKey]: action.filterValue }
-    );
-
-    var newFilters = objectAssign(
-      {},
-      state.filters,
-      { [action.filterType]: newFilter }
-    );
-    console.log(newFilters);
-
-    return objectAssign({}, state, { filters: newFilters } );
   default:
     return state;
   }
+}
+
+var filterVisibility = function(state={city: true}, action) {
+  switch(action.type){
+  case "INITIALIZE_FROM_SERVER":
+    return action.initialState.filterVisibility;
+  case "TOGGLE_FILTER_VISIBILITY":
+    var newSetting = {};
+    newSetting[action.filterKey] = !state[action.filterKey];
+
+    return objectAssign(
+      {},
+      state,
+      newSetting
+    );
+  default:
+    return state;
+  }
+}
+
+var tableHeadings = function(state = [], action) {
+  switch(action.type){
+  case "INITIALIZE_FROM_SERVER":
+    return action.initialState.tableHeadings;
+  default:
+    return state;
+  }
+}
+
+var records = function(state = [], action) {
+  switch(action.type){
+  case "INITIALIZE_FROM_SERVER":
+    return action.initialState.records;
+  default:
+    return state;
+  }
+}
+
+var labels = function(state={city: {}}, action) {
+  switch(action.type){
+  case "INITIALIZE_FROM_SERVER":
+    return action.initialState.labels;
+  default:
+    return state;
+  }
+}
+
+module.exports = function(state = {}, action) {
+  console.log(action);
+  return {
+    tableHeadings: tableHeadings(state.tableHeadings, action),
+    records: records(state.records, action),
+    filters: filters(state.filters, action),
+    filterVisibility: filterVisibility(state.filterVisibility, action),
+    labels: labels(state.labels, action)
+  };
 }
