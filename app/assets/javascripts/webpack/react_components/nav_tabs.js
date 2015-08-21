31b3d5a904f5d23d54f0d81dc989a1e55f754376
Brian Gracie <bgracie@gmail.com>
2015-08-21 00:53:26 -0700

    Style navbar

--- app/assets/javascripts/webpack/react_components/nav_tabs.js
+++ app/assets/javascripts/webpack/react_components/nav_tabs.js
@@ -3,13 +3,13 @@ var React = require("react");
 module.exports = React.createClass({
   render: function() {
     return (
-      <ul className="nav nav-tabs">
+      <ul className="nav nav-tabs" style={{marginLeft: "0px"}}>
         {
           this.props.tabs.map((tab, index) => {
             return(
               <li onClick={this.props.onTabClick(tab.key)}
                 key={index}>
-                { tab.label }
+                <a>{ tab.label }</a>
               </li>
             );
           })
