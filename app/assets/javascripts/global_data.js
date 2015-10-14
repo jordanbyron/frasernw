efe03c8b95c644453a7f474f6f5d535adc45a0b7
Brian Gracie <bgracie@gmail.com>
2015-10-14 12:13:32 -0700

    Introduce local storage cache versioning

--- app/assets/javascripts/global_data.js
+++ app/assets/javascripts/global_data.js
@@ -1,36 +1,36 @@
 (function(pathways){
 
-  var dateNum = function() {
+  var expiryKey = function(cacheVersion) {
     var date = new Date()
 
-    return (date.getDate().toString() + date.getMonth().toString() + date.getYear().toString());
+    return (date.getDate().toString() + date.getMonth().toString() + date.getYear().toString() + cacheVersion.toString());
   };
 
-  var fetchGlobalData = function(deferred) {
+  var fetchGlobalData = function(deferred, cacheVersion) {
     $.get("/data_tables/global_data").done(function(data) {
       deferred.resolve(data);
 
       window.localStorage.pathwaysGlobalData = JSON.stringify(data);
-      window.localStorage.pathwaysGlobalDataExpiration = dateNum();
+      window.localStorage.pathwaysGlobalDataExpiration = expiryKey(cacheVersion);
     })
   };
 
-  var isGlobalDataSet = function() {
-    return (window.localStorage.pathwaysGlobalDataExpiration === dateNum() &&
+  var isGlobalDataSet = function(cacheVersion) {
+    return (window.localStorage.pathwaysGlobalDataExpiration === expiryKey(cacheVersion) &&
       window.localStorage.pathwaysGlobalData != undefined);
   };
 
-  pathways.setGlobalDataPromise = function(){
+  pathways.setGlobalDataPromise = function(cacheVersion){
     pathways.globalDataLoaded = function(){
       var deferred = $.Deferred();
 
-      if (isGlobalDataSet()) {
+      if (isGlobalDataSet(cacheVersion)) {
         deferred.resolve(
           JSON.parse(window.localStorage.pathwaysGlobalData)
         );
       } else {
         $(document).ready(function() {
-          fetchGlobalData(deferred);
+          fetchGlobalData(deferred, cacheVersion);
         });
       }
 
