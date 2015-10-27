65d22387986919d655b25ea9497f026fe018f8f4
Brian Gracie <bgracie@gmail.com>
2015-10-26 18:04:56 -0700

    Relabel and remove logging

--- app/assets/javascripts/webpack/react_components/usage_report.js
+++ app/assets/javascripts/webpack/react_components/usage_report.js
@@ -105,7 +105,6 @@ module.exports = React.createClass({
     );
   },
   render: function() {
-    console.log(this.props);
     return(
       <LoadingContainer
         isLoading={this.props.isLoading}
