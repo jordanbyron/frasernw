a863bbdc3739e96e835e8c8f96a1987e52c4f2b6
Brian Gracie <bgracie@gmail.com>
2015-11-02 19:27:59 -0800

    Allow super admins to see old resource data

--- app/assets/javascripts/webpack/state_mappers/usage_report.js
+++ app/assets/javascripts/webpack/state_mappers/usage_report.js
@@ -4,6 +4,12 @@ var React = require("react");
 var RadioButtons = require("../react_components/radio_buttons");
 var Selector = require("../react_components/selector");
 
+
+var isDataDubious = function(filterValues) {
+  return moment(filterValues.months, "YYYYMM").isBefore("2015-11-01", "months") &&
+    _.includes(["physicianResources", "forms", "patientInfo"], filterValues.recordTypes)
+}
+
 module.exports = function(stateProps: Object, dispatchProps: Object): Object {
   var state = stateProps;
   var dispatch = dispatchProps.dispatch;
@@ -30,8 +36,8 @@ module.exports = function(stateProps: Object, dispatchProps: Object): Object {
       noticeText: `${_.startCase(filterValues.recordTypes )} page views are only available for November 2015 and later.`,
       dispatch: dispatch,
       isLoading: false,
-      isPeriodValid: !(moment(filterValues.months, "YYYYMM").isBefore("2015-11-01", "months") &&
-        _.includes(["physicianResources", "forms", "patientInfo"], filterValues.recordTypes)),
+      shouldShowData: (state.app.currentUser.isSuperAdmin || !isDataDubious(filterValues)),
+      isDataDubious: isDataDubious(filterValues),
       isTableLoading: _.get(state, ["ui", "isTableLoading"], false),
       showTableHeart: moment(filterValues.months, "YYYYMM").isSame(moment(), "month"),
       query: requestNewData,
