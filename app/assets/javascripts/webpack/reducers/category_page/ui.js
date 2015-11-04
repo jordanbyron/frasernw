94da91d91abf869df489b7908295804f7c0420e1
Brian Gracie <bgracie@gmail.com>
2015-11-04 15:20:26 -0800

    Get subcategory filter working

--- app/assets/javascripts/webpack/reducers/category_page/ui.js
+++ app/assets/javascripts/webpack/reducers/category_page/ui.js
@@ -1,23 +1,21 @@
 import hasBeenInitialized from "reducers/has_been_initialized";
 import pageRenderedKey from "reducers/page_rendered_key";
 import sortConfig from "reducers/filter_table/sort_config";
+import filterValues from "reducers/filter_table/filter_values";
+import filterGroupVisibility from "reducers/filter_table/filter_group_visibility";
 import _ from "lodash";
 
 const contentCategoryId = _.partial(pageRenderedKey, "contentCategoryId");
 const pageType = _.partial(pageRenderedKey, "pageType");
 
 export default function(state = {}, action) {
+  console.log(action);
   return {
     hasBeenInitialized: hasBeenInitialized(state.hasBeenInitialized, action),
     contentCategoryId: contentCategoryId(state.contentCategoryId, action),
     pageType: pageType(state.pageType, action),
     sortConfig: sortConfig(state.sortConfig, action),
+    filterGroupVisibility: filterGroupVisibility(state.filterGroupVisibility, action),
+    filterValues: filterValues(state.filterValues, action),
   };
-  // return {
-  //   hasBeenInitialized: hasBeenInitialized(state.hasBeenInitialized, action),
-  //   filterValues: filterValues(state.filterValues, action),
-  //   filterVisibility: filterVisibility(state.filterVisibility, action),
-  //   rows: rows(state.rows, action),
-  //   isTableLoading: isTableLoading(state.isTableLoading, action)
-  // };
 }
--- app/assets/javascripts/webpack/state_mappers/category_page.js
+++ app/assets/javascripts/webpack/state_mappers/category_page.js
@@ -4,14 +4,21 @@ import RowGenerators from "state_mappers/filter_table/row_generators";
 import itemsForCategory from "domain/content_category_items";
 import SortFunctions from "state_mappers/filter_table/sort_functions";
 import SortOrders from "state_mappers/filter_table/sort_orders";
+import Filters from "state_mappers/filter_table/filters"
 
 export default function(state, dispatch) {
   console.log(state);
 
-  let _sortConfig = sortConfig(state);
   if(state.ui.hasBeenInitialized) {
+    const _divisionIds = divisionIds(state);
+    const _maskingSet =
+      itemsForCategory(state.ui.contentCategoryId, state, _divisionIds);
+    const _sortConfig = sortConfig(state);
+    const _filterValues =
+      _.mapValues(FilterValues, (value) => value(state, _maskingSet));
+
     return {
-      bodyRows: bodyRows(state, dispatch, _sortConfig),
+      bodyRows: bodyRows(state, dispatch, _sortConfig, _maskingSet, _filterValues),
       resultSummary: { isVisible: false },
       cityFilterPills: { shouldDisplay: false },
       specializationFilterMessage: { shouldDisplay: false },
@@ -23,7 +30,7 @@ export default function(state, dispatch) {
       filters: {
         title: `Filter ${state.app.contentCategories[state.ui.contentCategoryId].name}`,
         // TODO: name this 'sidebarFilteringSectionGroups'
-        groups: []
+        groups: _.map(FilterGroups, (group) => group(state, _filterValues)),
       },
       dispatch: dispatch
     }
@@ -33,22 +40,70 @@ export default function(state, dispatch) {
   }
 };
 
-const bodyRows = (state, dispatch, sortConfig) => {
-  if(state.app.currentUser.isSuperAdmin) {
-    var _divisionIds = _.values(state.app.divisions).map(_.property("id"));
-  }
-  else {
-    var _divisionIds = state.app.currentUser.divisionIds;
-  }
+const operativeFilterKeys = ["subcategories", "specialties"];
+const bodyRows = (state, dispatch, sortConfig, maskingSet, filterValues) => {
+  const _operativeFilters = _.values(_.pick(Filters, operativeFilterKeys))
+    .filter((filter) => filter.isActivated(filterValues))
+    .map(_.property("predicate"));
 
   return from(
     _.partialRight(_.sortByOrder, SortFunctions.contentCategories(sortConfig), SortOrders.contentCategories(sortConfig)),
     _.partialRight(_.map, _.partial(RowGenerators.resources, state.app, dispatch, { panelKey: 0 })),
-    itemsForCategory(state.ui.contentCategoryId, state, _divisionIds)
+    _.partialRight(_.filter, (record) => _.every(_operativeFilters, (filter) => filter(record, filterValues))),
+    maskingSet
   );
+};
+
+const divisionIds = (state) => {
+  if(state.app.currentUser.isSuperAdmin) {
+    return _.values(state.app.divisions).map(_.property("id"));
+  }
+  else {
+    return state.app.currentUser.divisionIds;
+  }
 }
 
 const sortConfig = (state) => ({
   order: _.get(state, ["ui", "sortConfig", "order"], "DOWN"),
   column: _.get(state, ["ui", "sortConfig", "column"], "TITLE"),
 })
+
+const FilterValues = {
+  subcategories: function(state, maskingSet) {
+    return _.chain(maskingSet)
+     .map((record) => record.categoryId)
+     .flatten()
+     .uniq()
+     .without(state.ui.contentCategoryId)
+     .reduce((memo, id) => {
+       return _.assign(
+         { [id]: _.get(state, ["ui", "filterValues", "subcategories", id], false) },
+         memo
+       );
+     }, {})
+     .value();
+  },
+}
+
+const FilterGroups = {
+  subcategories: function(state: Object, filterValues: Object): Object {
+    return {
+      filters: {
+        subcategories: _.map(
+          filterValues.subcategories,
+          function(value: boolean, subcategoryId: string) {
+            return {
+              filterId: subcategoryId,
+              label: state.app.contentCategories[subcategoryId].name,
+              value: value
+            };
+          }
+        ),
+      },
+      title: "Subcategories",
+      isOpen: _.get(state, ["ui" ,"filterGroupVisibility", "subcategories"], true),
+      shouldDisplay: _.any(_.keys(filterValues.subcategories)),
+      componentKey: "subcategories",
+    };
+  },
+}
