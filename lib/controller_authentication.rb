7d6af59f42c1848a5160c122bd7595c53b0c16ad
Brian Gracie <bgracie@gmail.com>
2015-11-13 23:56:21 -0800

    Handle case that someone's logged in and editing with the token saved on referent

--- lib/controller_authentication.rb
+++ lib/controller_authentication.rb
@@ -73,7 +73,7 @@ module ControllerAuthentication
   end
 
   def token_required(klass, token, id)
-    @secret_token_id = SecretToken.where(token: token).first.id
+    @secret_token_id = SecretToken.where(token: token).first.try(:id)
 
     unless klass.find(id).valid_tokens.include?(token)
       redirect_to login_url, :alert => "Invalid token. Please email millerjc@shaw.ca to request or reset your secret url for editing."
