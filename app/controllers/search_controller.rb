ce0dde914e53de718879f7dd6dd4df06a1c87941
Brian Gracie <bgracie@gmail.com>
2015-12-09 16:08:18 -0800

    Disable http caching for livesearch all entries as we are serving a stale cache

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,10 @@ class SearchController < ApplicationController
 
   def livesearch_all_entries
     respond_to do |format|
+      response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+
       format.js
     end
   end
