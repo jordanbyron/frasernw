1ceca75cfc3a02891412e718f618828ea5b592cd
kelseydh <kelseyh@gmail.com>
2015-12-09 17:15:27 -0800

    don't assume division relationship exists for news_item subscription activities Fix #259

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   load_and_authorize_resource
   def index
-    @notifications = PublicActivity::Activity.order("created_at DESC").includes(trackable: :division).all
+    @notifications = PublicActivity::Activity.order("created_at DESC").all
     #@notifications = PublicActivity::Activity.order("created_at DESC").where(owner_id: current_user.division, owner_type: "Admin")
   end
 
