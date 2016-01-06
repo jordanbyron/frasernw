6b83b1e89c5be62dbcce5b261df87b1fb0c270b0
kelseydh <kelseyh@gmail.com>
2016-01-05 20:32:50 -0800

    add system notifications if specialists are deleted

--- app/services/remove_deceased_specialist_records.rb
+++ app/services/remove_deceased_specialist_records.rb
@@ -2,7 +2,13 @@ class RemoveDeceasedSpecialistRecords < ServiceObject
   def call
     Specialist.deceased.each do |specialist|
       if specialist.version_marked_deceased.created_at < 2.years.ago
-        specialist.destroy
+        sname = specialist.name
+        sid = specialist.id
+        if specialist.destroy
+          SystemNotifier.notice("Deceased Specialist deleted successfully: #{sid} #{sname}")
+        else
+          SystemNotifier.notice("ERROR deleting deceased specialist: #{sid} #{sname}")
+        end
       end
     end
   end
