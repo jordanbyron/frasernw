630d5e77a5140bd40d2ac2000f8bf95aeb85df8a
Brian Gracie <bgracie@gmail.com>
2016-01-13 14:58:02 -0800

    Replace old latest updates

--- app/helpers/front_helper.rb
+++ app/helpers/front_helper.rb
@@ -1,6 +1,6 @@
 module FrontHelper
   def latest_events(max_automated_events, divisions)
-    NewLatestUpdates.call(
+    LatestUpdates.call(
       max_automated_events: max_automated_events,
       division_ids: divisions.map(&:id)
     )
