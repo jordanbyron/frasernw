6e617b8384ca78e26f07f4ba30fa10b7ba5635f4
Brian Gracie <bgracie@gmail.com>
2016-01-14 14:31:06 -0800

    Debug

--- app/helpers/front_helper.rb
+++ app/helpers/front_helper.rb
@@ -2,7 +2,7 @@ module FrontHelper
   def latest_events(max_automatic_events, divisions)
     LatestUpdates.call(
       max_automatic_events: max_automatic_events,
-      division_ids: divisions.not_hidden.map(&:id)
+      division_ids: divisions.reject(&:hidden?)
     )
   end
 end
