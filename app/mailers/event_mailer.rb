12db3bbb70500b85e21b92c4b2b8be32d5c5fe27
Brian Gracie <bgracie@gmail.com>
2016-03-04 16:29:41 -0800

    Re: #349: Make it clear in review queue email whether changes were made

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -16,8 +16,21 @@ class EventMailer < ActionMailer::Base
 
   def mail_review_queue_entry(review_item)
     @review_item = review_item
+    @verb = @review_item.no_updates? ? "confirmed as accurate" : "edited"
+    @whodunnit = begin
+      if @review_item.secret_edit?
+        "using a secret edit link"
+      else
+        "by #{@review_item.editor.name}"
+      end
+    end
+
     item = review_item.item
-    mail(:to => item.owners.map{|owner| owner.email}, :from => 'noreply@pathwaysbc.ca', :subject => "Pathways: #{item.name} has had been edited and is in the review queue")
+    mail(
+      :to => item.owners.map{|owner| owner.email},
+      :from => 'noreply@pathwaysbc.ca',
+      :subject => "Pathways: #{item.name} has had been edited and is in the review queue"
+    )
   end
 
 end
