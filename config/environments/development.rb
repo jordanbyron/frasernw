c7f93f2d3612f8b44f4e6f6ade65df1f9dfc416a
Brian Gracie <bgracie@gmail.com>
2016-03-14 16:05:47 -0700

    Dev aesthetics

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,36 +20,33 @@ Frasernw::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = (ENV["BENCHMARKING"].to_b || false)
+  if ENV["CACHE_CLASSES"] == "true"
+    config.cache_classes = true
+
+    puts "==> Application code will not be reloaded on every request."
+  else
+    config.cache_classes = false
+  end
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = (ENV["CACHE"].to_b || false)
-  config.cache_store = :dalli_store, { :value_max_bytes => 10485760 }
+  config.consider_all_requests_local  = true
 
-  # # # # # Development Feature Switches:
-  # Use to gain more production-like configs in local development; turns cache_classes & perform_caching to true, or includes livesearch.js
-  # To use switches, call them when booting a local rails server, e.g.:
-  # $~> BENCHMARKING=true CACHE=true LIVESEARCH=true BULLET=true rails s
-  if ENV["BENCHMARKING"].to_b == true
-    puts "==> BENCHMARKING TURNED ON -- REPEAT REQUESTS CACHE CODE TO IMITATE PRODUCTION"
+  if ENV["CACHE"] == "true"
+    config.action_controller.perform_caching = true
+    config.cache_store = :dalli_store, { :value_max_bytes => 10485760 }
+
+    puts "==> Rails cache is activated.  Make sure you have memcached on (`memcached -I 5m &`)."
+  else
+    config.action_controller.perform_caching = false
+    config.cache_store = :null_store
   end
 
-  if ENV["CACHE"].to_b == true
-    puts "==> PERFORM CACHING TURNED ON -- MEMCACHE IS ON TO IMITATE PRODUCTION"
+  if ENV["NAVBAR_SEARCH"] == "true"
+    puts "==> Top nav search enabled."
   end
-
-  if ENV["LIVESEARCH"].to_b == true
-    puts "==> LIVESEARCH.js file will be included"
-  end
-
-  if ENV["BULLET"].to_b == true
-    puts "==> BULLET GEM ENABLED --- DETECTED N+1 QUERIES NOW TRIGGER WARNINGS"
-  end
-  # # # # #
 
   # Don't care if the mailer can't send
   # config.action_mailer.raise_delivery_errors = false
@@ -70,17 +67,18 @@ Frasernw::Application.configure do
   Rails.application.middleware.use Oink::Middleware
 
   config.after_initialize do
-    #bullet actions only run if explicitly enabled
-    Bullet.enable = (ENV["BULLET"].to_b || false)
-    #Bullet.alert = false
+    if ENV["BULLET"] == "true"
+      Bullet.enable = true
+
+      puts "==> Bullet gem enabled.  (Bullet logs n+1 queries.)"
+    else
+      false
+    end
+
     Bullet.bullet_logger = true
     Bullet.console = true
-    ##Bullet.growl = true
     Bullet.rails_logger = true
-    ## Bullet.bugsnag = true
-    ## #Bullet.airbrake = true
     Bullet.add_footer = true
-    ##Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
   end
 
   config.action_mailer.smtp_settings = {
--- config/initializers/pry.rb
+++ config/initializers/pry.rb
@@ -1,16 +1,15 @@
-# Colour rails console pry shell prompt differently based on environment
 if ENV['APP_NAME'] == "pathwaysbc"
   color = :red
-  warning = "Production "
+  warning = "[Production] "
 elsif !Rails.env.development?
   color = :yellow
   warning = ""
 elsif Rails.env.development?
-  color = :cyan
+  color = :green
   warning = ""
 end
 
-colored_env = Pry::Helpers::Text.send(color, ENV['APP_NAME'])
-colored_warning = Pry::Helpers::Text.send(color, warning)
-
-Pry.config.prompt_name = "#{colored_warning}pry@#{colored_env}"
+Pry.config.prompt_name = Pry::Helpers::Text.send(
+  color,
+  "#{warning}pry@#{ENV['APP_NAME']}"
+)
