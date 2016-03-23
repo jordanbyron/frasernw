a3135731265502a78718f9f440186b3e77b72699
Brian Gracie <bgracie@gmail.com>
2016-03-22 21:31:14 -0700

    Include the original body

--- lib/pathways_dev_mailer.rb
+++ lib/pathways_dev_mailer.rb
@@ -4,15 +4,16 @@ class PathwaysDevMailer
 
   def deliver!(mail)
     new_recipients = ENV['SYSTEM_NOTIFICATION_RECIPIENTS']
-    new_body = "<p>Original Recipients: #{mail.to}</p><hr>"
+    new_body = "<p>Original Recipients: #{mail.to}</p><hr><br>#{mail.body}"
     new_subject = "[DevMail][#{ENV['APP_NAME']}] #{mail.subject}"
 
     mail.to = new_recipients
     mail.body = new_body
     mail.subject = new_subject
+    mail.content_type = "text/html"
 
     smtp_settings = Frasernw::Application.config.action_mailer.smtp_settings
-    
+
     ActionMailer::Base.delivery_methods[:smtp].new(smtp_settings).deliver!(mail)
   end
 end
