8e7428ddb7207a01018c6c68c7b3e5a205ace069
Brian Gracie <bgracie@gmail.com>
2016-03-22 21:09:54 -0700

    Manually instantiate and call the smtp mailer

--- lib/pathways_dev_mailer.rb
+++ lib/pathways_dev_mailer.rb
@@ -1,13 +1,18 @@
-class PathwaysDevMailer < Mail::SMTP
+class PathwaysDevMailer
+  def initialize(params = {})
+  end
+
   def deliver!(mail)
     new_recipients = ENV['SYSTEM_NOTIFICATION_RECIPIENTS']
     new_body = "<p>Original Recipients: #{mail.to}</p><hr>"
-    new_subject = "[Dev Mail][#{ENV['APP_NAME']}] #{mail.subject}"
+    new_subject = "[DevMail][#{ENV['APP_NAME']}] #{mail.subject}"
 
     mail.to = new_recipients
     mail.body = new_body
     mail.subject = new_subject
 
-    super(mail)
+    smtp_settings = Frasernw::Application.config.action_mailer.smtp_settings
+    
+    ActionMailer::Base.delivery_methods[:smtp].new(smtp_settings).deliver!(mail)
   end
 end
