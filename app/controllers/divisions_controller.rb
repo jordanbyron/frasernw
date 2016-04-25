beed48a150ea1276a746f31e95e0974d3894d97e
Brian Gracie <bgracie@gmail.com>
2016-04-25 12:54:44 -0700

    Fix sharing from specific divisions

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -149,7 +149,7 @@ class DivisionsController < ApplicationController
   private
 
   def authorize_division_for_user
-    if !(current_user.as_super_admin? || (current_user.as_divisions.include? Division.find(params[:id])))
+    if !current_user.as_super_admin? && !current_user.as_divisions.include?(Division.find(params[:id]))
       redirect_to root_url, :notice => "You are not allowed to access this page"
     end
   end
