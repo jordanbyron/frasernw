3697b15bdd3169994ecb9af1bac2f18d6bec2f5e
Brian Gracie <bgracie@gmail.com>
2016-05-20 15:42:46 -0700

    Refactor filter group

--- app/assets/javascripts/webpack/component_helpers/filter_group.js
+++ app/assets/javascripts/webpack/component_helpers/filter_group.js
@@ -2,21 +2,19 @@ import React from 'react';
 import { buttonIsh } from "stylesets";
 import _ from "lodash";
 
-const FilterGroup = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <div className="filter_group__title open" key="title">
-          <span>{ this.props.title }</span>
-        </div>
-        <div className="filter_group__filters" key="filters">
-          <div style={{paddingTop: "12px", paddingBottom: "12px"}}>
-            { this.props.children }
-          </div>
+const FilterGroup = ({title, children}) => {
+  return (
+    <div>
+      <div className="filter_group__title open" key="title">
+        <span>{ title }</span>
+      </div>
+      <div className="filter_group__filters" key="filters">
+        <div style={{paddingTop: "12px", paddingBottom: "12px"}}>
+          { children }
         </div>
       </div>
-    );
-  }
-});
+    </div>
+  );
+};
 
 export default FilterGroup;
