92d982a6065585ec69c186d4a3bd3964377da471
dhMuse <danielmusekamp@icloud.com>
2016-05-27 23:21:47 -0700

    WIP on direct s3 uploads re: #435, to avoid app timeouts.

--- db/migrate/20160513030708_create_videos.rb
+++ db/migrate/20160513030708_create_videos.rb
@@ -2,6 +2,7 @@ class CreateVideos < ActiveRecord::Migration
   def change
     create_table :videos do |t|
       t.string :title
+      t.string :video_url
       t.attachment :video_clip
 
       t.timestamps
