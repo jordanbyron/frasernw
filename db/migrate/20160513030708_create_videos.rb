edc1d18109b9bfc8b719b5437d272ecb719579fe
dhMuse <danielmusekamp@icloud.com>
2016-05-28 19:18:38 -0700

    Remove paperclip methods from videos, re: @92d982a6065585ec69c186d4a3bd3964377da471.

--- db/migrate/20160513030708_create_videos.rb
+++ db/migrate/20160513030708_create_videos.rb
@@ -3,7 +3,6 @@ class CreateVideos < ActiveRecord::Migration
     create_table :videos do |t|
       t.string :title
       t.string :video_url
-      t.attachment :video_clip
 
       t.timestamps
     end
