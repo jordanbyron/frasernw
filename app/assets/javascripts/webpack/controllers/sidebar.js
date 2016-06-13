abd83012aee619bb07125531e4df90bcec7559a3
dhMuse <danielmusekamp@icloud.com>
2016-06-13 11:15:12 -0700

    Reorder sidebar filters to prioritize 'subcategories' above 'specializations'.

--- app/assets/javascripts/webpack/controllers/sidebar.js
+++ app/assets/javascripts/webpack/controllers/sidebar.js
@@ -50,6 +50,7 @@ const Sidebar = ({model, dispatch}) => {
     }>
       <div className="well filter">
         <div className="title">{ sidebarTitle(model) }</div>
+        <SubcategoriesFilters model={model} dispatch={dispatch}/>
         <ProcedureFilters model={model} dispatch={dispatch}/>
         <SpecializationsFilters model={model} dispatch={dispatch}/>
         <ReferralsFilters model={model} dispatch={dispatch}/>
@@ -64,7 +65,6 @@ const Sidebar = ({model, dispatch}) => {
         <LanguagesFilters model={model} dispatch={dispatch}/>
         <AssociationsFilters model={model} dispatch={dispatch}/>
         <CityFilters model={model} dispatch={dispatch}/>
-        <SubcategoriesFilters model={model} dispatch={dispatch}/>
         <HiddenUpdatesFilter model={model} dispatch={dispatch}/>
         <MonthFilter model={model} dispatch={dispatch}/>
         <ReportStyleFilter model={model} dispatch={dispatch}/>
