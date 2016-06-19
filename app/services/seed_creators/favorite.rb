1979136231495f31b1bc6b7f4ee48ee6621524c8
Brian Gracie <bgracie@gmail.com>
2016-06-19 14:16:24 -0700

    Modifications to seed data

--- app/services/seed_creators/favorite.rb
+++ app/services/seed_creators/favorite.rb
@@ -1,11 +1,4 @@
 module SeedCreators
-  class Favorite < SeedCreator::HandledTable
-    Handlers = {
-      user_id: :pass_through,
-      created_at: :pass_through,
-      updated_at: :pass_through,
-      favoritable_id: :pass_through,
-      favoritable_type: :pass_through,
-    }
+  class Favorite < SeedCreator::SkippedTable
   end
 end
