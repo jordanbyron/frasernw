cc3579961e90728913c02e1db01c3befda74eae2
dhMuse <danielmusekamp@icloud.com>
2016-08-05 18:00:45 -0700

    Find users' issue_assignments to allow associated record destruction on user destroy.

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,7 @@ class User < ActiveRecord::Base
   has_many :divisions, through: :user_divisions
 
   has_many :favorites
-  has_many :issue_assignments, dependent: :destroy
+  has_many :issue_assignments, dependent: :destroy, foreign_key: :assignee_id
   has_many :assigned_issues,
     through: :issue_assignments,
     class_name: "Issue"
