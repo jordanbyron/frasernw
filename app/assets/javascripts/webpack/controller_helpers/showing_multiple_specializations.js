6b1cfebfabe99fdcdd6fb34f18b9ee394f993b4c
Brian Gracie <bgracie@gmail.com>
2016-09-17 13:20:43 -0700

    Show specialties column on aop#show

--- app/assets/javascripts/webpack/controller_helpers/showing_multiple_specializations.js
+++ app/assets/javascripts/webpack/controller_helpers/showing_multiple_specializations.js
@@ -4,7 +4,8 @@ import { showingOtherSpecializations } from "controller_helpers/filter_messages"
 const showingMultipleSpecializations = (model) => {
   return showingOtherSpecializations(model) ||
     matchedRoute(model) === "/hospitals/:id" ||
-    matchedRoute(model) === "/languages/:id"
+    matchedRoute(model) === "/languages/:id" ||
+    matchedRoute(model) === "/areas_of_practice/:id"
 }
 
 export default showingMultipleSpecializations;
