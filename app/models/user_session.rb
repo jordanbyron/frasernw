93cb192706f89e4113babb83abf98de245daf733
dhMuse <danielmusekamp@icloud.com>
2016-09-20 19:30:47 -0700

    Close REQ#187: make consecutive_failed_logins_limit an ENV variable.

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,5 +1,5 @@
 class UserSession < Authlogic::Session::Base
-  self.consecutive_failed_logins_limit 5
+  self.consecutive_failed_logins_limit ENV["FAILED_LOGIN_LIMIT"].to_i || 5
   self.failed_login_ban_for 2.hours
   self.logout_on_timeout = true
 end
