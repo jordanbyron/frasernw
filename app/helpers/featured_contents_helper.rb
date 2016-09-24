339f7bbbb8b895cac9f1bb34f15a05723cd87324
Brian Gracie <bgracie@gmail.com>
2016-09-24 12:55:55 -0700

    Don't have headings for optgroups

--- app/helpers/featured_contents_helper.rb
+++ app/helpers/featured_contents_helper.rb
@@ -1,9 +1,7 @@
 module FeaturedContentsHelper
-  def featured_contents_options(items)
-    [
-      ["NEW", items.select(&:new?)],
-      ["Not New", items.reject(&:new?)]
-    ].select{|option| option[1].any? }
+  def featured_contents_options(category, division)
+    category.
+      all_sc_items_in_divisions([division]).
+      sort_by{|item| [ (item.new? ? 0 : 1), item.title ] }
   end
-
 end
