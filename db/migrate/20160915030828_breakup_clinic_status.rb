b8a43aba0092393220885ae7321c6b96ea040e28
Brian Gracie <bgracie@gmail.com>
2016-10-04 21:48:01 -0400

    Clinic form

--- db/migrate/20160915030828_breakup_clinic_status.rb
+++ db/migrate/20160915030828_breakup_clinic_status.rb
@@ -6,7 +6,7 @@ class BreakupClinicStatus < ActiveRecord::Migration
     add_column :clinics, :accepting_new_referrals, :boolean
     add_column :clinics, :referrals_limited, :boolean
 
-    add_column :clinics, :availability, :integer
+    add_column :clinics, :is_open, :boolean
 
     Clinic.all.each do |clinic|
       clinic.without_versioning do
@@ -18,12 +18,9 @@ class BreakupClinicStatus < ActiveRecord::Migration
     end
   end
 
-  NEW_SPECIALIST_ATTRIBUTES = {
-    surveyed: -> (clinic) {
-      clinic.categorization_mask != 3 #purposely not surveyed
-    },
-    responded_to_survey: ->(clinic) {
-      #purposely not surveyed, not responded to survey
+  NEW_CLINIC_ATTRIBUTES = {
+    returned_completed_survey: ->(clinic){
+      #purposely not surveyed, not responded
       ![2, 3].include?(clinic.categorization_mask)
     },
     accepting_new_referrals: ->(clinic){
@@ -34,36 +31,16 @@ class BreakupClinicStatus < ActiveRecord::Migration
       clinic.categorization_mask == 1 && #responded to survey
         clinic.status_mask == 7 #accepting limited new referrals
     },
-    availability: -> (clinic){
+    is_open: ->(clinic){
       case clinic.status_mask
-      when 1 # accepting new referrals
-        Specialist::AVAILABILITY_LABELS.key(:available)
-      when 2 # only doing follow up
-        Specialist::AVAILABILITY_LABELS.key(:available)
-      when 4 #retired as of
-        Specialist::AVAILABILITY_LABELS.key(:retired)
-      when 5 #retiring as of
-        Specialist::AVAILABILITY_LABELS.key(:available)
-      when 6 #unavailable between
-        if clinic.unavailable_from < Date.today && clinic.unavailable_to > Date.today
-          Specialist::AVAILABILITY_LABELS.key(:temporarily_unavailable)
-        else
-          Specialist::AVAILABILITY_LABELS.key(:available)
-        end
-      when 7 #didn't answer
-        Specialist::AVAILABILITY_LABELS.key(:unknown)
-      when 8 #indefinitely unavailable
-        Specialist::AVAILABILITY_LABELS.key(:indefinitely_unavailable)
-      when 9 #permanently unavailable
-        Specialist::AVAILABILITY_LABELS.key(:permanently_unavailable)
-      when 10 #moved away
-        Specialist::AVAILABILITY_LABELS.key(:moved_away)
-      when 11 #limited referrals
-        Specialist::AVAILABILITY_LABELS.key(:available)
-      when 12 #deceased
-        Specialist::AVAILABILITY_LABELS.key(:deceased)
+      when 3
+        nil
+      when nil
+        nil
+      when 4
+        false
       else
-        Specialist::AVAILABILITY_LABELS.key(:available)
+        true
       end
     }
   }
