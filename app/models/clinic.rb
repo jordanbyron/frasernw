8f39e59c70460c7f639aeae987b495ce411fba9a
Brian Gracie <bgracie@gmail.com>
2016-10-07 19:03:01 -0400

    Only show attendances at well-formed clinic locations

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -589,12 +589,6 @@ class Clinic < ActiveRecord::Base
     name
   end
 
-  def visible_attendances
-    @visible_attendances ||= attendances.select do |attendance|
-      attendance.show?
-    end
-  end
-
   alias_method :cities_for_front_page, :cities
 
   def specialists_with_offices_in
@@ -619,5 +613,12 @@ class Clinic < ActiveRecord::Base
       map(&:hospital_in).
       reject(&:nil?).
       uniq
+  end
+
+  def locations_showing_attendances
+    @locations_showing_attendances ||= clinic_locations.select do |location|
+      location.resolved_address.present? &&
+        location.attendances.select(&:show?).any?
+    end
   end
 end
