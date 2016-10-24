9e95710a8f0c55350d9844d6fb63f83db1075f6e
dhMuse <danielmusekamp@icloud.com>
2016-10-24 00:56:24 -0700

    WIP on CR#66.

--- db/migrate/20161014040421_create_divisional_resource_subscriptions.rb
+++ db/migrate/20161014040421_create_divisional_resource_subscriptions.rb
@@ -2,7 +2,8 @@ class CreateDivisionalResourceSubscriptions < ActiveRecord::Migration
   def change
     create_table :divisional_resource_subscriptions do |t|
       t.references :division
-      t.references :specialization
+      t.text :specialization_ids, array: true, default: []
+      t.boolean :nonspecialized, default: false
 
       t.timestamps
     end
