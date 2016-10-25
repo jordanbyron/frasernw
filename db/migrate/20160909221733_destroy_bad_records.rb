04041cade8f6d8f01dae138e9e97253ea50fc34d
Brian Gracie <bgracie@gmail.com>
2016-10-25 19:51:15 -0400

    Fix destroying bad records

--- db/migrate/20160909221733_destroy_bad_records.rb
+++ db/migrate/20160909221733_destroy_bad_records.rb
@@ -1,10 +1,16 @@
 class DestroyBadRecords < ActiveRecord::Migration
   def change
-    Focus.where('clinic_id > 1004').each{ |f| f.delete }
+    Focus.all.select do |focus|
+      focus.clinic.nil?
+    end.map(&:destroy)
+
+    Capacity.all.select do |capacity|
+      capacity.specialist.nil?
+    end.map(&:destroy)
 
     procedure_specialization_ids = ProcedureSpecialization.all.map(&:id)
-    ScItemSpecializationProcedureSpecialization.all.reject do |sisps|
+    ScItemSpecializationProcedure.all.reject do |sisps|
       procedure_specialization_ids.include?(sisps.procedure_specialization_id)
-    end.each{ |sisps| sisps.delete }
+    end.each(&:destroy)
   end
 end
