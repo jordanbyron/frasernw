61c5ae093b8b1f44874e8b3e46a06afc09a27d5d
dhMuse <danielmusekamp@icloud.com>
2016-10-24 17:22:35 -0700

    WIP on CR#66.

--- app/services/seed_creators/divisional_resource_subscription.rb
+++ app/services/seed_creators/divisional_resource_subscription.rb
@@ -2,7 +2,8 @@ module SeedCreators
   class DivisionalResourceSubscription < SeedCreator::HandledTable
     Handlers = {
       division_id: :pass_through,
-      specialization_id: :pass_through,
+      specialization_ids: :pass_through,
+      nonspecialized: :pass_through,
       created_at: :pass_through,
       updated_at: :pass_through,
     }
