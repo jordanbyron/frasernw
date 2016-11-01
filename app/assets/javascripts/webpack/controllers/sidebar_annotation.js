59302f6cbec262e50cb958bff9272195b1beecb7
Brian Gracie <bgracie@gmail.com>
2016-10-31 23:05:57 -0400

    Harmonize referral icon tooltips and key

--- app/assets/javascripts/webpack/controllers/sidebar_annotation.js
+++ app/assets/javascripts/webpack/controllers/sidebar_annotation.js
@@ -1,13 +1,11 @@
 import React from "react";
-import SpecialistIconKey from "controllers/specialist_icon_key";
-import ClinicIconKey from "controllers/clinic_icon_key";
+import ReferralIconKey from "controllers/referral_icon_key";
 import EntityPageViewsKey from "controllers/entity_page_views_key";
 
 const SidebarAnnotation = ({model, dispatch}) => {
   return(
     <div>
-      <SpecialistIconKey model={model}/>
-      <ClinicIconKey model={model}/>
+      <ReferralIconKey model={model}/>
       <EntityPageViewsKey model={model}/>
     </div>
   )
