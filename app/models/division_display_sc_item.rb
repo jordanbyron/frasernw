70543ccbb4bfdaf813fb4e5ed39ce592995cc922
dhMuse <danielmusekamp@icloud.com>
2016-11-14 22:24:03 -0800

    Destroy featured_content when division stops displaying the content.

--- app/models/division_display_sc_item.rb
+++ app/models/division_display_sc_item.rb
@@ -2,6 +2,12 @@ class DivisionDisplayScItem < ActiveRecord::Base
   belongs_to :division
   belongs_to :sc_item
 
+  has_one :featured_content_selection,
+    -> (display_item){ where(sc_item_id: display_item.sc_item_id) },
+    through: :division,
+    source: :featured_contents,
+    dependent: :destroy
+
   include PaperTrailable
 
 end
