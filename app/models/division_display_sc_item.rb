c71d28f992762377c9a1933dbe02bed19efc5500
dhMuse <danielmusekamp@icloud.com>
2016-11-15 01:26:01 -0800

    Debug @70543ccbb4bfdaf813fb4e5ed39ce592995cc922.

--- app/models/division_display_sc_item.rb
+++ app/models/division_display_sc_item.rb
@@ -3,11 +3,17 @@ class DivisionDisplayScItem < ActiveRecord::Base
   belongs_to :sc_item
 
   has_one :featured_content_selection,
-    -> (display_item){ where(sc_item_id: display_item.sc_item_id) },
+    -> (display_item){ where(
+      sc_item_id: display_item.sc_item_id,
+      division_id: display_item.division_id
+    ) },
     through: :division,
-    source: :featured_contents,
-    dependent: :destroy
+    source: :featured_contents
 
   include PaperTrailable
+
+  before_destroy do
+    self.featured_content_selection.destroy
+  end
 
 end
--- app/models/featured_content.rb
+++ app/models/featured_content.rb
@@ -9,9 +9,18 @@ class FeaturedContent < ActiveRecord::Base
 
   scope :in_category, -> (category) { where(sc_category_id: category.id) }
 
+  after_commit :flush_cache
+  after_touch :flush_cache
+
   def self.in_divisions(divisions)
     where('"division_id" IN (?)', divisions.map(&:id))
   end
 
   MAX_FEATURED_ITEMS = 4
+
+  private
+
+  def flush_cache
+    ExpireFragment.call "featured_content_#{self.division_id}"
+  end
 end
