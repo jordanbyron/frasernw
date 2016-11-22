3c442374dbcd3f9c417ca193aca90fcab9256967
Brian Gracie <bgracie@gmail.com>
2016-11-21 23:35:25 -0500

    Support breaking news

--- app/helpers/news_items_helper.rb
+++ app/helpers/news_items_helper.rb
@@ -1,8 +1,15 @@
 module NewsItemsHelper
   def render_news_item(news_item)
     case news_item.type_mask
-    when NewsItem::TYPE_DIVISIONAL
-      content_tag(:div, class: "news_item") do
+    when NewsItem::TYPE_DIVISIONAL, NewsItem::TYPE_BREAKING
+      classname =
+        if news_item.type_mask == NewsItem::TYPE_BREAKING
+          "news_item breaking"
+        else
+          "news_item"
+        end
+
+      content_tag(:div, class: classname) do
         content_tag(:div, news_item.date, class: "news_item_date") +
         content_tag(:div, news_item.title, class: FrontHelper.news_item_class(news_item)) +
         content_tag(:div, BlueCloth.new(news_item.body).to_html.html_safe, class: "news_item_body")
