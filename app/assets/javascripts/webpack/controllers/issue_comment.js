5b5997bbd9afdd7f3382d3b93871bf621373d0ab
dhMuse <danielmusekamp@icloud.com>
2016-12-12 16:29:13 -0800

    Spiffy up editing notes.

--- app/assets/javascripts/webpack/controllers/issue_comment.js
+++ app/assets/javascripts/webpack/controllers/issue_comment.js
@@ -2,17 +2,35 @@ import React from "react";
 
 const IssueComment = React.createClass({
   getInitialState: function() {
+    var note_component = this;
+    document.addEventListener("click", function() {
+      note_component.deselect();
+    })
     return _.assign({editing: false}, this.props);
   },
   edit: function() {
-    this.setState({editing: true});
+    if ((this.state.creator_id === this.state.current_user_id) &&
+      (!this.state.editing)) {
+        this.setState({editing: true});
+      }
   },
-  onContentChange: function(event){
+  deselect: function() {
+    var note_text = this;
+    var save_triggered = false;
+    $('#save_btn').mousedown(function() {
+      save_triggered = true;
+    });
+    $('.comment_input').blur(function() {
+      if (!save_triggered) {
+        note_text.setState({editing: false});
+      }
+    });
+  },
+  onContentChange: function(event) {
     this.setState({raw_content: event.target.value});
   },
   save: function() {
     var component = this;
-
     $.ajax({
       url: `/notes/${this.state.id}`,
       type: "PATCH",
@@ -28,7 +46,9 @@ const IssueComment = React.createClass({
   },
   render: function() {
     return(
-      <div className="well" style={{width: "400px"}}>
+      <div className="well" style={{width: "400px"}}
+        onClick={this.edit}
+      >
         <div style={{display: "inline-block"}}>
           <b>{ this.state.author_name }</b>
         </div>
@@ -36,26 +56,19 @@ const IssueComment = React.createClass({
           <i>{ this.state.created_at }</i>
         </div>
         {
-          !this.state.editing ?
+          this.state.editing ?
+            <div>
+              <textarea className="comment_input"
+                style={{width: "390px", height: "100px"}}
+                autoFocus={true}
+                value={this.state.raw_content}
+                onChange={this.onContentChange}
+              />
+              <div className="btn" id="save_btn" onClick={this.save}>Save</div>
+            </div> :
             <div style={{clear: "both", marginTop: "10px"}}
               dangerouslySetInnerHTML={{__html: this.state.content}}
-            /> : <div></div>
-        }
-        {
-          this.state.editing ?
-            <textarea value={this.state.raw_content}
-              onChange={this.onContentChange}
-            /> :
-            <div></div>
-        }
-        {
-          (this.state.creator_id === this.state.current_user_id) &&
-            (!this.state.editing) ?
-            <div onClick={this.edit}>Edit</div> : <div></div>
-        }
-        {
-          this.state.editing ?
-            <div onClick={this.save}>Save</div> : <div></div>
+            />
         }
       </div>
     )
